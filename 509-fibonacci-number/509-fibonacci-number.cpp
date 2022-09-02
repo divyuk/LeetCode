6e2a326cf88cb07d093c98146af8180f7c7a303e
divyuk <112690602+divyuk@users.noreply.github.com>
2022-09-02 22:04:36 +0530

    Time: 4 ms (50.96%), Space: 6 MB (39.37%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -6,13 +6,18 @@ public:
         return  dp[n] = f(n-1,dp)+f(n-2,dp);
     }
     int fib(int n) {
-        vector<int>dp(n+1,-1);
+        // vector<int>dp(n+1,-1);
         // return f(n , dp);
         if(n<2) return n;
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2 ; i<=n;i++)
-            dp[i] = dp[i-1]+dp[i-2];
-        return dp[n];
+        // dp[0] = 0;
+        // dp[1] = 1;
+        int prev = 1;
+        int prev2 = 0;
+        for(int i = 2 ; i<=n;i++){
+            int curr =  prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 };
