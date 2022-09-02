195ef7e26ca70d210052d18497251b09397c0af6
divyuk <112690602+divyuk@users.noreply.github.com>
2022-09-02 21:56:42 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (8.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    int f(int n){
+    int f(int n,vector<int>&dp){
         if(n<2) return n;
-        return f(n-1)+f(n-2);
+        if(dp[n]!=-1) return dp[n];
+        return  dp[n] = f(n-1,dp)+f(n-2,dp);
     }
     int fib(int n) {
-        return f(n);
+        vector<int>dp(n+1,-1);
+        return f(n , dp);
     }
 };
