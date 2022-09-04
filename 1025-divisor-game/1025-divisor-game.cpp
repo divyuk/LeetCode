821a0aa7fa323731f4fb2fe7b188ec99a3d13fac
divyuk <112690602+divyuk@users.noreply.github.com>
2022-09-04 11:58:03 +0530

    Time: 3 ms (46.82%), Space: 6.2 MB (13.05%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int calc(int n){
-        for (int i = 1; i < n; i++)
-        {
+    bool f(int n){
+        if(n==1) return false;
+        for(int i =1 ; i<n ; i++ ){
             if(n%i==0){
-                n = n-i;
-                return n;
+                if(f(n-i)==false) return true;
             }
         }
-        return -1;
-    }
-    int check(int n){
-        int n1 = calc(n);
-        if(n1==-1)
-            return 0;
-        return n1;
+        return false;
     }
     bool divisorGame(int n) {
-        while (1)
-        {
-            n = check(n);
-            if(n==0)
-                return false;
-            n = check(n);
-            if(n==0)
-                return true;
-        }
-        
+        vector<int>dp(n+1,-1);
+        // return f(n);
+        return n%2==0;
     }
 };
