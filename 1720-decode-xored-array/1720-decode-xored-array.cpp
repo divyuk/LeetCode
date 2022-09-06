24aad74c084328c619a6dbb0a6fa0c604c6283ae
divyuk <112690602+divyuk@users.noreply.github.com>
2022-09-06 20:43:17 +0530

    Time: 49 ms (51.13%), Space: 26 MB (35.32%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -14,3 +14,12 @@ public:
     }
 
 };
+// In Place xoring using reference
+//     vector<int> decode(vector<int>& encoded, int first) {
+// 	for (int &n : encoded) {
+// 		swap(n, first);
+// 		first ^= n;
+// 	}
+// 	encoded.push_back(first);
+// 	return encoded;
+// }
