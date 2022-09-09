d31943489f2e2c850265f40eb6d9bbbbd57d689c
divyuk <112690602+divyuk@users.noreply.github.com>
2022-09-09 19:36:20 +0530

    Time: 11 ms (39.20%), Space: 8.6 MB (76.51%) - LeetHub

--- 942-di-string-match/942-di-string-match.cpp
+++ 942-di-string-match/942-di-string-match.cpp
@@ -3,18 +3,14 @@ class Solution {
     vector<int> diStringMatch(string s) {
         int n = s.size();
         vector<int>ans(n+1);
-        int inc = n,dec=0;;
-        for(int i = 0;i<n;i++){
-            if(s[i]=='I'){
-                ans[i] = dec;
-                dec++;
-            }
-            else{
-                ans[i] = inc;
-                inc--;
-            }
+        int j = n;
+        int i = 0;
+        int k=0;
+        for(char c:s){
+            ans[k] = (c=='I')?i++:j--;
+            k++;
         }
-        ans[n] = inc;
+        ans[n] = j;
         return ans;
     }
 };
