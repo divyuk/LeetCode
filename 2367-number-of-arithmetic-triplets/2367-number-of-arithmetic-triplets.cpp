a07862076fa11daec677e12ea279b34768b32d50
divyuk <112690602+divyuk@users.noreply.github.com>
2022-09-12 16:39:41 +0530

    Time: 10 ms (59.34%), Space: 9 MB (20.88%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        unordered_set<int>u(nums.begin(),nums.end());
+        set<int>u(nums.begin(),nums.end());
         int count =0;
         for(auto i : u)
             if(u.find(i-diff)!=u.end() and u.find(i-2*diff)!=u.end())
