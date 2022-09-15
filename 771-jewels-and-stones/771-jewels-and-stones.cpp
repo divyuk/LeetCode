7692c0c7098f3b86bc3fe8c32327db11b8bcee90
divyuk <112690602+divyuk@users.noreply.github.com>
2022-09-15 17:58:28 +0530

    Time: 4 ms (45.23%), Space: 6.1 MB (77.03%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
         int c =0;
-        for(char j : jewels){
-            for(char s : stones){
-                if(j==s)
-                    c++;
-            }
-        }
+        bool arr[58]; //from 65to 122
+        memset(arr , 0 , sizeof(arr));
+        for(int i =0;i<jewels.size();i++)
+            arr[jewels[i]-65] = true;
+        for(int i = 0 ; i<stones.size() ; i++)
+            if(arr[stones[i]-65])
+                c++;
         return c;
     }
 };
