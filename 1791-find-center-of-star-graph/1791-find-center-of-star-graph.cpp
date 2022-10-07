58be9cf5b0c6c8cce6549bea1dcd23a36771fe95
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2022-10-07 13:53:47 +0530

    Time: 501 ms (22.16%), Space: 67.2 MB (84.58%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -4,10 +4,12 @@ public:
         int a =0,b=0;
         a = edges[0][0];
         b = edges[0][1];
-        if(edges[1][0] == a )
-            return a;
-        else if(edges[1][1] == a)
-            return a;
-        return b;
+        // if(edges[1][0] == a )
+        //     return a;
+        // else if(edges[1][1] == a)
+        //     return a;
+        // return b;
+        int k = edges[1][0] == a or edges[1][1] == a ? a : b;
+        return k;
     }
 };
