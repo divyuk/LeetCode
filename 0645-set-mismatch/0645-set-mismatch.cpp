db3eb46b51e1b869f772933909fdd4296b63c889
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2022-10-23 10:46:44 +0530

    Time: 279 ms (5.09%), Space: 21.5 MB (66.56%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int len = nums.size(), dup, mis;
+        for ( int index = 0; index < len; ++index )
+        {
+            if ( nums[abs(nums[index]) - 1] < 0 ){
+                dup = abs(nums[index]);
+                cout << dup<<"  "<< index<<"\n";
+            }
+            else nums[abs(nums[index]) - 1] *= -1;
+        }
+        for(auto i : nums)
+            cout << i <<" ";
+        
+        for ( int index = 0; index < len; ++index )
+        {
+            if ( nums[index] > 0 )
+            {
+                mis = index + 1;
+                break;
+            }
+        }
+        return {dup, mis};
+    }
+};
