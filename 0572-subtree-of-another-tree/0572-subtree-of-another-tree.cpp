b4687ba89e82b0a2173e27bdcf8a27b22370db45
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2022-11-26 11:44:06 +0530

    Time: 36 ms (77.55%), Space: 28.7 MB (82.08%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == NULL) {
+            return false;
+        }
+        if (isSame(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+private:
+    bool isSame(TreeNode* root, TreeNode* subRoot) {
+        if (root == NULL && subRoot == NULL) {
+            return true;
+        }
+        if (root == NULL || subRoot == NULL) {
+            return false;
+        }
+        if (root->val != subRoot->val) {
+            return false;
+        }
+        return isSame(root->left, subRoot->left) && isSame(root->right, subRoot->right);
+    }
+};
