2a6624b5826686fedfe5d0b6fe2e36c649fafa6f
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-01-09 13:35:08 +0530

    Time: 3 ms (37.93%), Space: 6.1 MB (25.60%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        vector<int>arr;
-        for(int i = 0; i<n; i++){
-            arr.push_back(start+2*i);
-        }
+        // vector<int>arr;
+        int num=0;
         int xr = 0;
-        
-        for(auto i : arr){
-            xr ^=i;
+        for(int i = 0; i<n; i++){
+            num = start+2*i;
+            xr^=num;
         }
+        
+        // for(auto i : arr){
+        //     xr ^=i;
+        // }
         return xr;
     }
 };
