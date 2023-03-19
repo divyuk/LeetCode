77d7d08fcf873339231629ec3d958b810798ab1b
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-03-19 09:15:02 +0530

    Time: 653 ms (75.02%), Space: 153.9 MB (84.45%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        queue<int>q;
-        vector<bool>visited(n,false);
-        vector<int>adj_list[n];
-        for(int i = 0 ;i < edges.size() ; i++){
-            int j = edges[i][0];
-            int k = edges[i][1];
-            adj_list[k].push_back(j);
-            adj_list[j].push_back(k);
+        vector<int>adj[n];
+        for(int i =0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        // for(auto i : adj_list){
-        //     for(auto l : i)
-        //         cout<<l<<" ";
-        //     cout<<"\n";
-        // }
+        
+        queue<int>q;
+        vector<bool>visited(n+1,false);
         q.push(source);
         visited[source] = true;
         while(!q.empty()){
-            int  u = q.front();
+            int src = q.front();
             q.pop();
-            for(auto v : adj_list[u]){
-                if(visited[v] == false){
-                    q.push(v);
-                    visited[v] = true;
+            for(auto i : adj[src]){
+                if(!visited[i]){
+                    visited[i]=true;
+                    q.push(i);
                 }
             }
         }
