c8b36ec989c5215de003c71cf72d1f68d93d33bb
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-03-19 09:30:30 +0530

    Time: 190 ms (77.37%), Space: 68.8 MB (33.28%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        // Concept of indegree and outdegree
         vector<int>data(n+1,0);
         for(auto i : trust){
             data[i[0]]--;
             data[i[1]]++;
         }
-        for(int i = 1;i<=n ;i++){
+        
+        for(int i=1;i<=n;i++){
             if(data[i]==n-1)
                 return i;
         }
