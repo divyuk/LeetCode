6bd130b3563512ba9df11f218be2f7f0c394d4a4
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-03-20 13:27:59 +0530

    Time: 791 ms (52.36%), Space: 112 MB (56.30%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
-    void mergeSort(vector<int>& nums, int l , int h , int &count){
-        if(l==h) return;
-        
-        int mid = (l+h)/2;
-        
-        mergeSort(nums,l,mid,count);
-        mergeSort(nums,mid+1,h,count);
-        
-        vector<int> temp(h-l+1,0);
-        int i = l, j = mid+1, k = 0;
-        
+    void merge(vector<int>& nums,int l,int mid , int h , int &count){
+        vector<int>temp(h-l+1,0);
+        int i =l,k=0,j=mid+1;
         while(i <= mid && j <= h){
             if(nums[i] <= 2LL*nums[j]){
                 i++;
@@ -30,20 +22,27 @@ public:
                 temp[k++] = nums[j++];
             }
         }
+        while(i<=mid) temp[k++] = nums[i++];
+        while(j<=h) temp[k++] = nums[j++];
         
-        while(i <= mid) temp[k++] = nums[i++];
-        while(j <= h)   temp[k++] = nums[j++];
-        
-        k = 0;
-        while(l <= h){
-            nums[l++] = temp[k++];
+        k=0;
+        while(l<=h) nums[l++] = temp[k++];
+    }
+    void mergeSort(vector<int>& nums,int l,int h , int &count){
+        if(l<h){
+            int mid = (l+h)/2;
+            mergeSort(nums,l,mid,count);
+            mergeSort(nums,mid+1,h,count);
+            merge(nums,l,mid,h,count);
+
         }
     }
-    
+
     int reversePairs(vector<int>& nums) {
-        int count = 0;
-        int n = nums.size()-1;
-        mergeSort(nums, 0, n, count);
+        int count =0;
+        int l =0,h=nums.size()-1;
+        mergeSort(nums,l,h,count);
+        for(int i : nums) cout<<i<<" ";
         return count;
     }
 };
