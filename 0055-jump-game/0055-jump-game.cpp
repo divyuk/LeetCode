3bcaa87e430468416a736a556ca19c5d65a50401
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-03-23 23:07:41 +0530

    Time: 69 ms (65.55%), Space: 48.4 MB (50.28%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int goal =0;
+        for(int i  = n-1 ; i>=0 ; i--){
+            if(nums[i]+i>=goal)
+                goal = i;
+            }
+        return true ? !goal : false;
+    }
+};
