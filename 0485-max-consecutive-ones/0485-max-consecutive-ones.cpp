9b34afc2e68eafe93aef4fb342d50eafa64a2acb
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-03-31 13:16:13 +0530

    Time: 41 ms (56.07%), Space: 36.2 MB (24.24%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -5,15 +5,9 @@ public:
         int r = nums.size();
         int c = 0,ans=0;
         while(l<nums.size()){  
-            if(nums[l] == 1)
-                c++;
-            else{
-               
-                c=0;
-            }
-            
+            c = (nums[l] == 1) ? ++c : 0;
             l++;
-             ans = max(ans , c);
+            ans = max(ans , c);
         }
         return ans;
     }
