d0e1ae97627667ea4ab1515b668bd9bbc72a25e1
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-03-31 19:56:50 +0530

    Time: 244 ms (92.61%), Space: 71.2 MB (76.22%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -11,19 +11,19 @@
  */
 class Solution {
 public:
-    void pre(TreeNode* root1, TreeNode* root2, int l){
-    if (root1 == NULL)
-        return;
-
-    if (l%2 == 0)
-        swap(root1->val, root2->val);
- 
-    pre(root1->left, root2->right, l+1);
-    pre(root1->right, root2->left, l+1);
+    void reverseMachine(TreeNode* root1,TreeNode* root2, int l){
+        if(root1==NULL)
+            return ;
+        if(l%2==0)
+            swap(root1->val , root2->val);
+        reverseMachine(root1->left, root2->right, l+1);
+        
+        reverseMachine(root1->right , root2->left,l+1);
     }
     
     TreeNode* reverseOddLevels(TreeNode* root) {
-        pre(root->left,root->right,0);
+        int level = 0;
+        reverseMachine(root->left , root->right,level);
         return root;
     }
 };
