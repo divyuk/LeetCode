c2c67c7196a08e99a3cf77f312bc65fb4c0b631a
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-08-25 12:33:47 +0530

    Time: 19 ms (92.68%), Space: 71.4 MB (30.80%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,30 +10,24 @@
  */
 class Solution {
 public:
-ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode* ans = new ListNode();
-    ListNode* current = ans;  // Use a separate pointer to traverse the answer linked list
-    int carry = 0;
-
-    while (l1 || l2 || carry) {
-        int sum = carry;
-        
-        if (l1) {
-            sum += l1->val;
-            l1 = l1->next;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry =0;
+        ListNode* ans = new ListNode(0);
+        ListNode *p = l1 , *q = l2 , *curr = ans;
+        while(p || q){
+            int x = (p)?p->val:0;
+            int y = (q)?q->val:0;
+            int sum = carry + x + y;
+            carry = sum/10;
+            curr->next  = new ListNode(sum%10);
+            curr = curr->next;
+            if(p)
+                p=p->next;
+            if(q)
+                q=q->next;
         }
-        
-        if (l2) {
-            sum += l2->val;
-            l2 = l2->next;
-        }
-        
-        carry = sum / 10;
-        current->next = new ListNode(sum % 10);
-        current = current->next;
+        if(carry)
+            curr->next = new ListNode(carry);
+        return ans->next;
     }
-
-    return ans->next;  // Skip the dummy node at the beginning
-}
-
 };
