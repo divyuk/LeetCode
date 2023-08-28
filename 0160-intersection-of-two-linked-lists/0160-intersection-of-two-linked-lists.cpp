b01e78cf9ef4a39e60231609877902f32c13b762
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-08-28 11:07:57 +0530

    Time: 31 ms (95.86%), Space: 14.7 MB (32.67%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,23 +6,50 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+
+class Solution
+{
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode* > uset;
-        
-        //Filling in set
-        
-        while(headA){
-            uset.insert(headA);
-            headA=headA->next;
+    int calculateDifference(ListNode *headA, ListNode *headB)
+    {
+        int lenA = 0, lenB = 0;
+        while (headA)
+        {
+            lenA++;
+            headA = headA->next;
         }
-        
-        while(headB){
-            if(uset.find(headB)!=uset.end()) return headB;
-            headB=headB->next;
+        while (headB)
+        {
+            lenB++;
+            headB = headB->next;
         }
-        
+
+        return lenA - lenB;
+    }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
+        //! Calculating the difference between length
+        int difference = calculateDifference(headA, headB);
+
+        if (difference < 0)
+            while (difference++ != 0)
+                headB = headB->next;
+        else
+            while (difference-- != 0)
+                headA = headA->next;
+
+        while (abs(difference) == 0)
+            headA = headA->next;
+
+        //! Running them together
+        while (headA)
+        {
+            if (headA == headB)
+                return headA;
+            headA = headA->next;
+            headB = headB->next;
+        }
         return NULL;
     }
 };
