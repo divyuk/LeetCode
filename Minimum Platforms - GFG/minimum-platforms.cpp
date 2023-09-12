50048bffba00d9ed9af265c3e43423a44feb275a
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-09-12 14:48:48 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -12,36 +12,29 @@ class Solution{
     //railway station such that no train waits.
     int findPlatform(int arr[], int dep[], int n)
     {
-        // Store the arrival and departure time
-        vector<pair<int, int> > arr2(n);
-     
-        for (int i = 0; i < n; i++) {
-            arr2[i] = { arr[i], dep[i] };
+        vector<pair<int,int>>trains;
+        for(int i=0;i<n;++i)
+            trains.push_back({arr[i], dep[i]});
+            
+        
+        sort(trains.begin() , trains.end());
+        priority_queue<int, vector<int>, greater<int>> trainChart;
+        
+        trainChart.push(trains[0].second);
+        
+        int platforms = 1;
+        
+        for(int i =1;i<n;++i){
+            
+            if(trainChart.top() >= trains[i].first )
+                platforms++;
+            else
+                trainChart.pop();
+            
+            trainChart.push(trains[i].second);
         }
-     
-        // Sort arr2 based on arrival time
-        sort(arr2.begin(), arr2.end());
-     
-        priority_queue<int, vector<int>, greater<int> > p;
-        int count = 1;
-        p.push(arr2[0].second);
-     
-        for (int i = 1; i < n; i++) {
-     
-            // Check if arrival time of current train
-            // is less than or equals to departure time
-            // of previous train
-            if (p.top() >= arr2[i].first) {
-                count++;
-            }
-            else {
-                p.pop();
-            }
-            p.push(arr2[i].second);
-        }
-     
-        // Return the number of trains required
-        return count;
+        
+        return platforms;
     }
 };
 
