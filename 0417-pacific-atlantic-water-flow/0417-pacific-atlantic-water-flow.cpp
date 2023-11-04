13db62fd084faa23e604a744243bd36381b22c01
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-11-04 12:44:53 +0530

    Time: 38 ms (41.69%), Space: 20.9 MB (28.47%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,58 +1,57 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<int>> result;
-        if (heights.empty() || heights[0].empty()) {
-            return result;
-        }
-        
+    vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights)
+    {
         int m = heights.size();
         int n = heights[0].size();
-        
-        // Create two boolean grids to mark if water can reach the Pacific and Atlantic Oceans.
+
         vector<vector<bool>> canReachPacific(m, vector<bool>(n, false));
         vector<vector<bool>> canReachAtlantic(m, vector<bool>(n, false));
-        
-        // DFS to check if water can flow from each cell to the Pacific Ocean.
-        for (int i = 0; i < m; i++) {
-            dfs(heights, canReachPacific, i, 0);
-            dfs(heights, canReachAtlantic, i, n - 1);
+        vector<vector<int>> result;
+
+        //? Check from Pacific Left --> and Atlantic <-- right
+
+        for (int i = 0; i < m; i++)
+        {
+            dfs(heights, canReachPacific, m, n, i, 0);
+            dfs(heights, canReachAtlantic, m, n, i, n - 1);
         }
-        
-        for (int j = 0; j < n; j++) {
-            dfs(heights, canReachPacific, 0, j);
-            dfs(heights, canReachAtlantic, m - 1, j);
+        for (int i = 0; i < n; i++)
+        {
+            dfs(heights, canReachPacific, m, n, 0, i);
+            dfs(heights, canReachAtlantic, m, n, m - 1, i);
         }
-        
-        // Check if water can flow to both the Pacific and Atlantic Oceans and add to the result.
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (canReachPacific[i][j] && canReachAtlantic[i][j]) {
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (canReachPacific[i][j] and canReachAtlantic[i][j])
                     result.push_back({i, j});
-                }
             }
         }
-        
+
         return result;
     }
-    
-    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& canReach, int r, int c) {
-        if (canReach[r][c]) {
+
+    void dfs(vector<vector<int>> &heights, vector<vector<bool>>& canReach, int m, int n, int r, int c)
+    {
+        if (canReach[r][c])
             return;
-        }
-        
         canReach[r][c] = true;
-        int m = heights.size();
-        int n = heights[0].size();
-        
-        // Check and explore neighboring cells.
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        for (const auto& dir : directions) {
+        vector<pair<int, int>> directions = { {1, 0},
+                                              {-1, 0},
+                                              {0, 1},
+                                              {0, -1} };
+
+        for (pair<int, int> &dir : directions)
+        {
             int newRow = r + dir.first;
-            int newCol = c + dir.second;
-            if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && heights[newRow][newCol] >= heights[r][c]) {
-                dfs(heights, canReach, newRow, newCol);
-            }
+            int newColumn = c + dir.second;
+
+            if (newRow >= 0 and newRow < m and newColumn >= 0 and newColumn < n and heights[r][c] <= heights[newRow][newColumn])
+                dfs(heights, canReach, m, n, newRow, newColumn);
         }
     }
 };
