b6870edee53b588125704931be5a968676c8d283
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-11-27 18:44:28 +0530

    Time: 10 ms (71.40%), Space: 20 MB (77.56%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+class Solution {
+public:
+    int trap(std::vector<int>& height) {
+        int n = height.size();
+        if (n <= 2) {
+            return 0; // Not enough bars to trap water
+        }
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int trappedWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = std::max(leftMax, height[left]);
+                trappedWater += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = std::max(rightMax, height[right]);
+                trappedWater += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return trappedWater;
+    }
+};
