8fde9accef541ff022d847123af6897bfcdbdfed
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-12-04 16:33:53 +0530

    Time: 7 ms (41.75%), Space: 11 MB (58.17%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    void create(int start, vector<int>& nums, vector<int>& container, int target) {
+        if (target==0) {
+            result.push_back(container);
+            return;
+        }
+        for (int i = start; i < nums.size(); i++) {
+            // Skip duplicates
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            if(nums[i] > target) break;
+            // Include the current element in the combination
+            container.push_back(nums[i]);
+            create(i + 1, nums, container, target-nums[i]);
+            // Backtrack
+            container.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        vector<int>container;
+        sort(nums.begin() , nums.end());
+        create(0,nums,container, target);
+        return result;
+    }
+};
