313185720e6651e2e72402c04b7eb65f1f37f2a5
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-12-06 13:09:28 +0530

    Time: 2 ms (49.74%), Space: 6.6 MB (6.05%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int s = 1, ans = 0;
-        while (n > 0) {
-            for (int i = 0; i < 7 && n-- > 0; ++i) {
-                ans += s + i;
-                cout<< ans <<"\n";
-            }
-            cout<<"---\n";
-            s++;
-        }
-        return ans;
+          //completeWeek indicates how many complete week Hercy added money
+        //to the bank.
+        int completeWeek = n / 7;
+        
+        //leftDays indicates number of left days after completeWeek turn.
+        int leftDays = n % 7;
+        
+        //starting money in the last week
+        int startMoney = completeWeek + 1;
+        
+        //sum of AP series : Sn = (n * [2 * a + (n - 1) * d]) / 2
+        int completeWeekMoneysum = (completeWeek * (2 * 28 + (completeWeek - 1) * 7)) / 2;
+        
+        int leftDaysMoneySum = (leftDays * (2 * startMoney + (leftDays - 1) * 1)) / 2;
+        
+        return (completeWeekMoneysum + leftDaysMoneySum);
     }
 };
