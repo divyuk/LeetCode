77573b8db33c9320d408be9324c918632ff92cab
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-12-06 10:15:49 +0530

    Time: 19 ms (13.68%), Space: 6.3 MB (70.53%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int s = 1, ans = 0;
+        while (n > 0) {
+            for (int i = 0; i < 7 && n-- > 0; ++i) {
+                ans += s + i;
+                cout<< ans <<"\n";
+            }
+            cout<<"---\n";
+            s++;
+        }
+        return ans;
+    }
+};
