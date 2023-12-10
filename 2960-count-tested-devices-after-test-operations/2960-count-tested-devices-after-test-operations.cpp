ebe8a4511cdde95b2dedf2fe67b7ba66e5f20852
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-12-10 11:46:46 +0530

    Time: 0 ms (100.00%), Space: 17.8 MB (16.67%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    int countTestedDevices(vector<int>& bp) {
-        int n = bp.size();
-        int c=0;
-        for(int i =0;i<n;i++){
-            int curr = bp[i];
-            if(curr>0) c++;
-            else continue;
-            for(int j = i+1 ; j<n;j++){
-                if(bp[j] >0) 
-                    bp[j]-=1;
-        
-            } 
-        }
-        return c;
+    int countTestedDevices(vector<int>& bP) {
+        int k =0;
+        for(int i:bP )
+            k+=i>k;
+        return k;
     }
 };
