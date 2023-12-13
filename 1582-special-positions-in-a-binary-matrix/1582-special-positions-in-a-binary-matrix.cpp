e8891cd38a9669727844cf0148194f6bf12ddd31
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-12-13 11:46:31 +0530

    Time: 12 ms (90.00%), Space: 13.2 MB (46.58%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,24 +1,32 @@
-class Solution {
 
-public:    
-    int numSpecial(vector<vector<int>>& mat) {
-        
-        vector<int> rows(mat.size()), cols(mat[0].size());
-        
-        for (int i = 0; i < rows.size(); ++i){
-            for (int j = 0; j < cols.size(); ++j) {
-                if (mat[i][j])
-                    ++rows[i], ++cols[j];
+class Solution {
+public:
+    int numSpecial(std::vector<std::vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<int> rowSum(m, 0);
+        vector<int> colSum(n, 0);
+        int count = 0;
+
+        // First pass: Count the number of 1s in each row and each column
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 1) {
+                    rowSum[i]++;
+                    colSum[j]++;
+                }
             }
         }
-        int res = 0;
-        
-        for (int i = 0; i < rows.size(); ++i){
-            for (int j = 0; j < cols.size(); ++j){
-                if (mat[i][j] && rows[i] == 1 && cols[j] == 1)
-                    ++res;
+
+        // Second pass: Check if the current element is the only 1 in its row and column
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1) {
+                    count++;
+                }
             }
         }
-        return res;
+
+        return count;
     }
 };
