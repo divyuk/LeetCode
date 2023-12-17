5aefb5e14e9fced29e45ab2f97f79bd9ee4ff877
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-12-17 16:02:04 +0530

    Time: 156 ms (10.00%), Space: 115.2 MB (10.00%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,44 +1,12 @@
-
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>> result;
         sort(nums.begin(), nums.end());
-        for(int i : nums) cout<<i<<" ";
-        int n = nums.size();
-        int i = 0;
-
-        while (i < n) {
-            vector<int> currentArray;
-            int j = i;
-
-            // Build each subarray with exactly three elements
-            while (j < n && currentArray.size() < 3) {
-                if (currentArray.empty() || nums[j] - currentArray.back() <= k and nums[j] - currentArray[0] <=k) {
-                    currentArray.push_back(nums[j]);
-                } else {
-                    // If the difference condition is not satisfied, it cannot form a valid array
-                    return {};
-                }
-                j++;
-            }
-
-            if (currentArray.size() != 3) {
-                // If the size is not exactly 3, it cannot form a valid array
-                return {};
-            }
-
-            // Add the current array to the result
-            result.push_back(currentArray);
-
-            i = j; // Move to the next element after the current array
+        vector<vector<int>>ans;
+        for(int i = 2; i<nums.size() ; i+=3){
+            if(nums[i] - nums[i-2] > k) return {};
+            ans.push_back({nums[i-2] , nums[i-1] , nums[i]});
         }
-
-        // Check if there are any remaining elements that couldn't form a valid array
-        if (i < n) {
-            return {};
-        }
-
-        return result;
+        return ans;
     }
 };
