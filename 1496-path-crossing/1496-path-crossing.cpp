58f2bd521b7c811670d4f2a481a816f86eda1aeb
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2023-12-23 16:52:59 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (64.64%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -9,10 +9,8 @@ public:
             else if (c == 'S') y -= 1;
             else if (c == 'W') x -= 1;
             else if (c == 'E') x += 1;
-            // Check if the current position has been visited before
-            if (!s.insert({x, y}).second) {
-                return true;
-            }
+            if(s.find({x,y}) != s.end()) return true;
+            else s.insert({x,y});
         }
 
         return false;
