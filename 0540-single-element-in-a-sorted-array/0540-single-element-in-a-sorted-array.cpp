0ec48684219e9ad9d753b484a542b187b860548a
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-01-09 21:03:28 +0530

    Time: 22 ms (48.22%), Space: 22.7 MB (55.45%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,9 +3,9 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         int left = 0, right =n-1;
-        while(left < right) {
+        while(left < right) {  
+            int mid = (left + right) /2; // This is the mid value
             
-            int mid = (left + right) /2;
             if( (mid%2==0 and nums[mid] == nums[mid+1] ) or ( mid%2==1 and nums[mid] == nums[mid-1]) )
                 left = mid  +1;
             else
