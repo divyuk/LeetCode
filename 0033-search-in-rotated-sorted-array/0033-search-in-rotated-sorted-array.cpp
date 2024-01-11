13e3021f4e50778da96b86e06093388099d863cf
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-01-11 16:52:31 +0530

    Time: 3 ms (69.01%), Space: 11.3 MB (69.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
+        int n = nums.size();
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target) return mid;
+            
+            // Check if left half is sorted
+            else if(nums[low] <= nums[mid] ){
+                // this means element is definetaly in left half
+                if(nums[low] <= target and target < nums[mid])
+                    high = mid-1;
+                else
+                    low=mid+1;
+                
             }
-
-            // Check if the left half is sorted
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    // Target is in the left half
-                    high = mid - 1;
-                } else {
-                    // Target is in the right half
-                    low = mid + 1;
-                }
-            }
-            // Check if the right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    // Target is in the right half
-                    low = mid + 1;
-                } else {
-                    // Target is in the left half
-                    high = mid - 1;
-                }
+            else{
+                if(nums[mid]<target and target <= nums[high])
+                    low = mid +1;
+                else
+                    high = mid-1;
             }
         }
-
-        return -1; // Target not found
+        return -1;
     }
 };
