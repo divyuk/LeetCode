c17a7b0d04f9161d5c9039db6321e17f843d26aa
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-01-12 12:39:57 +0530

    Time: 23 ms (77.44%), Space: 89.4 MB (99.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       // Initialization some neccessary variables
-        vector<int>v;
-        
-        // store the array in the new array
-        for(auto num:nums1)   // O(n1)
-            v.push_back(num);
-        
-        for(auto num:nums2)  // O(n2)
-            v.push_back(num);
-        
-        // Sort the array to find the median
-        sort(v.begin(),v.end());  // O(nlogn)
-        
-        // Find the median and Return it
-        int n=v.size();  // O(n)
-        
-        return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        int m = a.size();
+        int n = b.size();
+        if(m>n) return findMedianSortedArrays(b,a); // First one to always sorter
+        int low = 0, high = m;
+        int total = m+n;
+        int left = (m + n + 1)/2; // no of elements to pick from array
+        while(low<=high){
+            int mid1 = (low+high)>>1; // mid1 no of element to from arr1
+            int mid2 = left-mid1; // remaining elemets from arr2
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX , r2 = INT_MAX;
+            if(mid1<m) r1 = a[mid1];
+            if(mid2<n) r2 = b[mid2];
+            if(mid1-1>=0) l1 = a[mid1-1];
+            if(mid2-1>=0)  l2 = b[mid2-1];
+            
+            if(l1<=r2 and l2<=r1){
+                if(total%2==1) return max(l1,l2);
+                else
+                return ((double)(max(l1,l2) + min(r1,r2) )) /2.0;
+            }
+            else if(l1>r2) high = mid1-1;
+            else 
+                low = mid1+1;
+        }
+        return 0;
     }
 };
