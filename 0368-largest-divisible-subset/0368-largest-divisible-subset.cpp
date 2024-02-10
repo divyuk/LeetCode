1f3bbfbf52643f5e55e57d115705f4aaa94dcd27
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-02-10 06:51:32 +0530

    Time: 17 ms (76.71%), Space: 11.4 MB (32.74%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,46 +1,55 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        if(nums.size()==0){
-            return nums;
-        }
-        sort(nums.begin(),nums.end());
-        int flag=1;
-        if(nums[0]==1){
-            flag=0;
-        }
-        else{
-            nums.push_back(1);
-            flag=1;
-        }
-        sort(nums.begin(),nums.end());
-        int i,j;
-        int dp[nums.size()][2];
-        for(i=nums.size()-1;i>=0;i--){
-            dp[i][0]=0;
-            dp[i][1]=i;
-            for(j=i+1;j<nums.size();j++){
-                if((nums[j]%nums[i])==0){
-                    if(dp[j][0]>dp[i][0]){
-                        dp[i][0]=dp[j][0];
-                        dp[i][1]=j;
-                    }  
+        
+        // Sorting: to leverage the transitive property!
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        // Initializing the DP and CHILD arrays
+        vector<int> dp(n, 1), child(n, -1);
+        
+        int imax = 0;
+        
+        for(int i = 1; i < n; i++) {
+            
+            // Considering ith element as the last element
+            // and finding the largest subset it can 
+            // belong to
+            for(int j = 0; j < i; j++) {
+                
+                // Using transitivity, if nums[i] % nums[j] == 0
+                // then all numbers in the subset ending at j
+                // will divide num[i] as well!
+                if(nums[i] % nums[j] == 0) {
+                    
+                    // Inclusion of i will increase the size of 
+                    // the subset by 1
+                    if(1 + dp[j] > dp[i]) {
+                        dp[i] = 1 + dp[j];
+                        
+                        // Setting the predecessor of i
+                        child[i] = j;
+                    }
                 }
             }
-            dp[i][0]++;
-        }
-        i=0;
-        vector<int> t;
-        t.push_back(nums[i]);
-        while(dp[i][1]!=i){
-            i=dp[i][1];
-            t.push_back(nums[i]);
             
+            // Determining the index where the largest subset ends
+            if(dp[i] > dp[imax]) {
+                imax = i;
+            }
         }
-        if(flag==1){
-            t.erase(t.begin());
-        }
-        return t;
         
+        vector<int> ans;
+        
+        // Backtracking to obtain the entire largest subset!
+        // This condition makes sure that the loop stops
+        // once the first element of the subset is traversed
+        while(imax != -1) {
+            ans.push_back(nums[imax]);
+            imax = child[imax];
+        }
+        
+        return ans;
     }
 };
