e5cdc215b0c37a382cd027805f940704c0ed5929
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-02-23 18:36:53 +0530

    Time: 19 ms (46.01%), Space: 17.4 MB (32.11%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    bool dfs(int crs, vector<bool>& visited, vector<bool>& cycle, vector<int>& ans, vector<vector<int>>& adj_list) {
-        if (visited[crs]) return cycle[crs]; // If already visited and in current path, it means cycle exists.
-        visited[crs] = true;
-        for (int p : adj_list[crs]) {
-            if (!dfs(p, visited, cycle, ans, adj_list)) return false;
-        }
-        cycle[crs] = true; // Mark in current path
-        ans.push_back(crs); // After traversing children, push the course to result.
-        return true;
-    }
-    
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj_list(numCourses); // Adjacency list for representing the graph
-        for (auto& pre : prerequisites) {
-            adj_list[pre[1]].push_back(pre[0]); // Constructing the adjacency list
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int>adj[n];
+        vector<int>indegree(n,0);
+        vector<int>ans;
+        for(auto it : pre){
+            int u = it[1];
+            int v = it[0];
+            adj[u].push_back(v);
+            indegree[v]++;
         }
         
-        vector<int> ans; // Result vector
-        vector<bool> visited(numCourses, false); // To track visited nodes
-        vector<bool> cycle(numCourses, false); // To track nodes in current path (for cycle detection)
-        
-        for (int i = 0; i < numCourses; ++i) {
-            if (!dfs(i, visited, cycle, ans, adj_list)) return {}; // If cycle detected, return empty array.
+        queue<int>q;
+        for(int i =0 ;i<n;i++){
+            if(indegree[i] == 0) q.push(i);
         }
         
-        reverse(ans.begin(), ans.end()); // Since we are pushing courses after visiting children, we need to reverse the result.
-        return ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        if(ans.size()==n) return ans;
+        return {};
     }
 };
