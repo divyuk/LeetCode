c2fca57f6efbe70ac71ba18e084e7fe10302ac78
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-03-19 10:13:23 +0530

    Time: 68 ms (56.23%), Space: 41.6 MB (47.49%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -3,24 +3,30 @@ public:
     int leastInterval(vector<char>& tasks, int k) {
         int n=tasks.size();
         unordered_map<char,int>mp;
+        // Put the count in map
         for(int i=0;i<tasks.size();i++){
             mp[tasks[i]]++;
         }
         priority_queue<int>pq;
         queue<pair<int,int>>q;
+        
+        // Put it in maxheap
         for(auto it:mp){
             pq.push(it.second);
         }
         int time=0;
+        // Start processing
         while(!pq.empty() || !q.empty()){
             time++;
             if(!pq.empty()){
-            int top=pq.top();
-            pq.pop();
-            if(top-1 !=0)
-            q.push({top-1,time+k});
+                int top=pq.top();
+                pq.pop();
+                if(top-1 !=0) // decrement the count as its processed and check if its
+                    // not zero, if its zero no need to process it.
+                    q.push({top-1,time+k});
             }
             auto front=q.front();
+            // if its now time to process it put in maxheap and process it.
             if(front.second==time){
                 pq.push(front.first);
                 q.pop();
@@ -29,3 +35,5 @@ public:
         return time;
     }
 };
+
+// TC : O(n*k) k is idle time , processing queue is O(log26)
