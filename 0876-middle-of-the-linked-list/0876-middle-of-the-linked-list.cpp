3c71d7eaf0431c0b98ecaba7cd97373e9a15a7bc
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-03-24 12:43:22 +0530

    Time: 4 ms (36.48%), Space: 8.7 MB (47.52%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -14,8 +14,8 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         while(fast and fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
         return slow;
     }
