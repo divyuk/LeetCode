a5f884bf3468ed89f0bb8163398cb37475055f27
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-03-24 10:15:06 +0530

    Time: 15 ms (13.64%), Space: 16.1 MB (75.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
     
-    void markRow(int row, int c , vector<vector<int>>& matrix){
-        for(int i = 0;i<c;i++) 
-            if(matrix[row][i]!=0)
-                matrix[row][i] = -999;
-
-    }
-    
-    void markCol(int col, int r , vector<vector<int>>& matrix){
-        for(int i = 0;i<r;i++)
-            if(matrix[i][col]!=0)
-                matrix[i][col] = -999;
-
-    }
-    
     void setZeroes(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
+        vector<int>rowArray(r,0);
+        vector<int>colArray(c,0);
         
         for(int i = 0; i<r;i++){
             for(int j =0 ; j<c;j++){
                 if(matrix[i][j] == 0){
-                    markRow(i,c,matrix);
-                    markCol(j,r,matrix);
+                    rowArray[i] = 1;
+                    colArray[j] = 1;
                 }
             }
         }
         
-        for(int i = 0; i<r;i++){
-            for(int j =0 ; j<c;j++){
-                if(matrix[i][j] == -999) matrix[i][j] = 0;
+      for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (rowArray[i] || colArray[j]) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        
-        
+
+
     }
 };
