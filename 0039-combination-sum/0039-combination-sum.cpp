3bc5abb282a7738b417fc687b6811a76555dd2ae
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-03-25 17:12:01 +0530

    Time: 96 ms (7.06%), Space: 67.1 MB (11.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
     
-    void create(int i,vector<int>&curr,vector<int>&c,int total,int target){
-        if(total==target){
-            ans.push_back(curr);
+    void combinator(int index,vector<int>& candidates, int target,vector<vector<int>>&result,int sum, vector<int>summer){
+        
+        if(sum == target){
+            result.push_back(summer);
             return;
         }
-        if(i>= c.size() or total>target) return;
-        curr.push_back(c[i]);
-        create(i,curr,c,total+c[i], target);
-        curr.pop_back();
-        create(i+1,curr,c,total, target);
+        
+        if(index >= candidates.size() or sum > target) return;
+        
+        summer.push_back(candidates[index]);
+        
+        combinator(index,candidates, target, result, sum+candidates[index],summer);
+        summer.pop_back();
+        combinator(index+1,candidates, target, result, sum,summer);
+
+        return;
     }
     
-    vector<vector<int>> combinationSum(vector<int>& c, int t) {
-        vector<int>curr;
-        create(0, curr,c,0,t);
-        return ans;
+    
+    
+    
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>result;
+        combinator(0,candidates,target, result,0,{}); // index
+        return result;
     }
 };
