a945399e959bc2b2011ed5b57cf50c2c9b5586f7
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-03-26 12:57:07 +0530

    Time: 22 ms (7.36%), Space: 10.3 MB (85.38%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -4,7 +4,7 @@ public:
         int count[128] = {};
         for (char c : s) count[c]++;
         sort(s.begin() , s.end() , [&](char a, char b){
-            return (count[a]>count[b] || (count[a]==count[b] and a<b) );
+            return (count[a]>count[b] || (count[a]==count[b] and a<b ) );
         });
         return s;
     }
