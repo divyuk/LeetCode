8eca5d319a0aee3d182ccd9190f61b5ff55ef0b1
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-03-27 10:31:27 +0530

    Time: 59 ms (50.04%), Space: 63.8 MB (13.98%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        if (k <= 1) {
-            return 0;
-        }
-        
-        int left = 0;
-        int product = 1;
-        int count = 0;
-        
-        for (int right = 0; right < nums.size(); right++) {
-            product *= nums[right];
-            
-            while (product >= k) {
-                product /= nums[left];
+        // Sliding window
+        if(k<=1) return 0;
+        int n = nums.size();
+        int left =0 , right =0, product =1, count =0;;
+        while( right< n){
+            product*=nums[right];
+            while(product>=k){
+                product/=nums[left];
                 left++;
             }
             
-            // Every time we move the right pointer,
-            // we add the size of the window to the count.
-            count += right - left + 1;
+            count+=right-left+1;
+            right++;
         }
         
         return count;
