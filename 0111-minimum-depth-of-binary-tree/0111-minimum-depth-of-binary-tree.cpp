7bb78a5de719139f47b2df49c75e4b20853e0e9f
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-03-30 11:03:16 +0530

    Time: 215 ms (5.52%), Space: 144.8 MB (33.32%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -12,32 +12,25 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        if (!root) return 0; // Check for empty tree
-
+        
+        if (!root) return 0; 
         queue<TreeNode*> q;
         q.push(root);
-        int depth = 1; // Start depth at 1, as we are counting nodes
+        
+        int depth = 1; 
 
         while (!q.empty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
+                if (!node->left && !node->right) 
+                    return depth; 
 
-                // Check if this node is a leaf
-                if (!node->left && !node->right) {
-                    return depth; // Return the current depth as soon as we find a leaf
-                }
-
-                // Add child nodes to the queue if they exist
-                if (node->left) {
-                    q.push(node->left);
-                }
-                if (node->right) {
-                    q.push(node->right);
-                }
+                if (node->left)  q.push(node->left);
+                if (node->right)  q.push(node->right);
             }
-            depth++; // Increment depth for each level of the tree
+            depth++; 
         }
         return depth;
     }
