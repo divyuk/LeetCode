ed51cb49f46b4a1a53ca0013bc4440fb348f0902
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-04 10:05:04 +0530

    Time: 11 ms (89.04%), Space: 25.7 MB (95.76%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -1,24 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        vector<TreeNode*> forest;
-        unordered_set<int> toDelete(to_delete.begin(), to_delete.end());
-        helper(root, toDelete, forest, true);
-        return forest;
+    
+    TreeNode* deleteHelper(TreeNode*root, unordered_set<int>&del, vector<TreeNode*>&result){
+        if(!root) return nullptr;
+        root->left = deleteHelper(root->left, del, result);
+        root->right = deleteHelper(root->right, del, result);
+        
+        if(del.find(root->val)!=del.end()){
+            if(root->left) result.push_back(root->left);
+            if(root->right) result.push_back(root->right);
+            return NULL;
+        }        
+        else return root;
     }
-
-private:
-    TreeNode* helper(TreeNode* node, unordered_set<int>& toDelete, vector<TreeNode*>& forest, bool isRoot) {
-        if (!node) return nullptr;
-
-        bool deleted = toDelete.find(node->val) != toDelete.end();
-        if (isRoot && !deleted) {
-            forest.push_back(node);
-        }
-
-        node->left = helper(node->left, toDelete, forest, deleted);
-        node->right = helper(node->right, toDelete, forest, deleted);
-
-        return deleted ? nullptr : node;
+    
+    
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        unordered_set<int>del(to_delete.begin(), to_delete.end());
+        vector<TreeNode*>result;
+        
+        deleteHelper(root,del,result);
+        if(del.find(root->val) == del.end() ) result.push_back(root);
+        return result;
     }
 };
