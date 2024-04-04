914d041ca06acf8984c0cda6bd9f2c7185a95bc4
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-04 07:04:03 +0530

    Time: 3 ms (40.00%), Space: 7.6 MB (22.85%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -4,12 +4,8 @@ public:
         int result=0,depth=0;
         stack<int>stk;
         for(char c : s){
-            if(c=='('){
-                depth++;
-            }
-            else if(c==')'){
-                depth--;
-            }
+            if(c=='(') depth++;
+            else if(c==')') depth--;
             result = max(result, depth);
         }
         return result;
