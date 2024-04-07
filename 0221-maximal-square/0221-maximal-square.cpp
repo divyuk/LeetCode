a591c5685374a16d69a7cd578afde602c7e28e7c
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-07 20:27:19 +0530

    Time: 54 ms (90.87%), Space: 22.1 MB (95.92%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
+    int maximalSquare(std::vector<std::vector<char>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
-         int maxsqlen = 0;
-        vector<vector<int>>dp(r+1, vector<int>(c+1,0));
-        for(int i = r-1; i>=0; i--){
-            for(int j =c-1 ; j>=0 ; j--){
-                if(matrix[i][j] == '1') {
-                    dp[i][j] = 1+ min(dp[i+1][j+1] , min(dp[i+1][j] , dp[i][j+1]) );
-                    maxsqlen = max(maxsqlen, dp[i][j]);
+        int maxsqlen = 0;
+        std::vector<int> dp(c + 1, 0);
+        int prev = 0; // Store the value of dp[i+1][j+1] from the previous iteration
+
+        for (int i = r - 1; i >= 0; i--) {
+            for (int j = c - 1; j >= 0; j--) {
+                int temp = dp[j]; // Store the current value of dp[j] before updating it
+                if (matrix[i][j] == '1') {
+                    dp[j] = 1 + std::min(prev, std::min(dp[j], dp[j + 1]));
+                    maxsqlen = std::max(maxsqlen, dp[j]);
+                } else {
+                    dp[j] = 0; // Reset to 0 if the current cell is '0'
                 }
-                
+                prev = temp; // Update prev for the next iteration
             }
         }
-        for(int i =0;i<=r; i++){
-            for(auto it : dp[i]) cout<<it<<" ";
-            cout<<"\n";
-        }
-        return maxsqlen*maxsqlen;
+
+        return maxsqlen * maxsqlen;
     }
 };
