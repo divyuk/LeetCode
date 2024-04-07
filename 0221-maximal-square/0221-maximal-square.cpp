84a164bd172fe5ce1e1b7dd8e04f1d3aaa4dcac7
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-07 20:19:55 +0530

    Time: 135 ms (11.56%), Space: 24.4 MB (55.53%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -5,14 +5,18 @@ public:
         int c = matrix[0].size();
          int maxsqlen = 0;
         vector<vector<int>>dp(r+1, vector<int>(c+1,0));
-        for(int i = 1; i<=r; i++){
-            for(int j =1 ; j<=c ; j++){
-                if(matrix[i-1][j-1] == '1') {
-                    dp[i][j] = 1+ min(dp[i-1][j-1] , min(dp[i-1][j] , dp[i][j-1]) );
+        for(int i = r-1; i>=0; i--){
+            for(int j =c-1 ; j>=0 ; j--){
+                if(matrix[i][j] == '1') {
+                    dp[i][j] = 1+ min(dp[i+1][j+1] , min(dp[i+1][j] , dp[i][j+1]) );
                     maxsqlen = max(maxsqlen, dp[i][j]);
                 }
                 
             }
+        }
+        for(int i =0;i<=r; i++){
+            for(auto it : dp[i]) cout<<it<<" ";
+            cout<<"\n";
         }
         return maxsqlen*maxsqlen;
     }
