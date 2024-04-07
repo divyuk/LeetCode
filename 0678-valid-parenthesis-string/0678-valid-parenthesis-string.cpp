b3dceecbc509b3ccd01be49a699303498d9842de
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-07 09:38:41 +0530

    Time: 3 ms (45.50%), Space: 7.5 MB (27.25%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -5,23 +5,18 @@ public:
         stack<int>stk2;
         for(int i =0;i<s.length() ; i++){
             char c = s[i];
-            if(c == '('){
-                stk1.push(i);
-            }
-            else if(c == '*'){
-                stk2.push(i);
-            }
+            if(c == '(') stk1.push(i);
+            
+            else if(c == '*') stk2.push(i);
+            
             
             else{
-                if(!stk1.empty())
-                    stk1.pop();
-                else if(!stk2.empty())
-                    stk2.pop();
+                if(!stk1.empty()) stk1.pop();
+                else if(!stk2.empty()) stk2.pop();
                 else return false;
             }
         }
-        // ((()))***
-        
+
         // ((*******
         
         while(!stk1.empty() and !stk2.empty()){
@@ -29,8 +24,6 @@ public:
             stk1.pop();
             stk2.pop();
         }
-        
-        // if(stk1.size() == stk2.size()  or (stk2.size()!=0 and stk1.size() ==0 ) ) return true;
         
         return stk1.empty();
     }
