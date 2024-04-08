23f80253ecb37364f33840e513bc4d7cd07bf27d
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-08 13:33:22 +0530

    Time: 48 ms (26.30%), Space: 50.9 MB (16.40%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,44 @@
 class Solution {
 public:
+    
+    int f(int idx, vector<int>&coins , int target, vector<vector<int>>cache){
+        if(idx==0){
+            if(target % coins[0]==0)  return target/coins[0];
+            return 1e9;
+        }
+        if(cache[idx][target]!=-1) return cache[idx][target];
+        
+        int notTake = f(idx-1, coins, target,cache);
+        int take=INT_MAX;
+        if(coins[idx] <= target) take = 1+ f(idx, coins, target-coins[idx],cache);
+        
+        return cache[idx][target] =  min(take,notTake);
+    }
+    
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX); // Initialize dp array with INT_MAX
-        dp[0] = 0; // Base case: 0 coins needed to make 0 amount
-
-        for (int j = 1; j <= amount; ++j) {
-            for (int i = 0; i < coins.size(); ++i) {
-                if (coins[i] <= j && dp[j - coins[i]] != INT_MAX) {
-                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
-                }
+        int n = coins.size();
+        vector<vector<int>>cache(n+1,vector<int>(amount+1, -1));
+        
+        for(int a = 0; a<=amount ; a++){
+            if(a%coins[0] == 0 ) cache[0][a] = a/coins[0];
+            else
+                cache[0][a] = 1e9;
+        }
+        
+        for(int i = 1 ; i< n; i++){
+            for(int t = 0; t<=amount; t++){
+                int notTake = cache[i-1][t];
+                int take=INT_MAX;
+                if(coins[i] <= t) take = 1+ cache[i][t-coins[i]];
+                cache[i][t] =  min(take,notTake);
             }
         }
-
-        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+        
+        return (cache[n-1][amount]>=1e9) ? -1 : cache[n-1][amount];
+        
+//         int ans= f(n-1, coins, amount, cache);
+      
+//         if(ans>=1e9) return -1;
+//         return ans;
     }
 };
