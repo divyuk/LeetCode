1d230529892ac6a38da3e1e0f66704693bede828
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-08 10:37:44 +0530

    Time: 66 ms (14.53%), Space: 9.1 MB (92.93%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,19 +1,41 @@
 class Solution {
+private :
+    int cache[301][11];
 public:
-    int minDifficulty(vector<int>& A, int D) {
-        int n = A.size(), inf = 1e9, maxd;
-        if (n < D) return -1;
-        vector<int> dp(n + 1, 1e9);
-        dp[n] = 0;
-        for (int d = 1; d <= D; ++d) {
-            for (int i = 0; i <= n - d; ++i) {
-                maxd = 0, dp[i] = inf;
-                for (int j = i; j <= n - d; ++j) {
-                    maxd = max(maxd, A[j]);
-                    dp[i] = min(dp[i], maxd + dp[j + 1]);
-                }
-            }
+    
+    int solve(vector<int>&jd, int n, int idx, int d){
+        // if you are at day 1 you have to complete all the remaining job
+        if(d==1)
+            return *max_element(jd.begin()+idx, jd.end());
+        
+        if(cache[idx][d] != -1) return cache[idx][d];
+        
+        int maxValue = INT_MIN;
+        int result = INT_MAX;
+        
+        /*
+            Try all possibilites 
+            by taking 1 job in one day
+            by taking 2 jon in one day
+            ...
+            
+        */
+        
+        for(int i = idx; i<=n-d ; i++){
+            maxValue  = max(maxValue, jd[i]);
+            result = min(result, maxValue+ solve(jd,n,i+1,d-1));
         }
-        return dp[0];
+        
+        return cache[idx][d] = result;
+    }
+    
+    
+    
+    int minDifficulty(vector<int>& jd, int d) {
+        int n = jd.size();
+        if(n<d) return -1;
+        memset(cache, -1, sizeof(cache));
+        return solve(jd,n,0,d); // 0 -> idx
+        
     }
 };
