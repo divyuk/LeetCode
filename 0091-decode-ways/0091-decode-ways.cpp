5b61eb4f05bba489d6dfd921780036b7772ace1e
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-09 11:07:50 +0530

    Time: 4 ms (31.61%), Space: 7.6 MB (78.29%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    int numDecodings(std::string s) {
-        int n = s.size();
-        if (n == 0 || s[0] == '0') {
-            return 0;
+    int solve(int i , int n , string &s, vector<int>&dp){
+        if(i==n)  return  dp[i] = 1;
+        if(s[i] == '0')  return dp[i] = 0;
+        if(dp[i] != -1) return dp[i];
+        
+        int onechar =solve(i+1,n,s, dp);
+        int twochar =0;
+        
+        if(i+1<n){
+            if(s[i] == '1' or (s[i] == '2' and s[i+1] <='6') ) 
+                twochar = solve(i+2, n, s, dp);
         }
-
-        // dp[i] represents the number of ways to decode the substring s[0:i].
-        std::vector<int> dp(n + 1, 0);
-        dp[0] = 1; // Empty string is considered as one way (base case).
-
-        for (int i = 1; i <= n; ++i) {
-            // Check for single-digit decoding.
-            if (s[i - 1] != '0') {
-                dp[i] += dp[i - 1];
-            }
-
-            // Check for two-digit decoding if possible.
-            if (i > 1 && isValidTwoDigit(s.substr(i - 2, 2))) {
-                dp[i] += dp[i - 2];
-            }
-        }
-
-        return dp[n];
+        
+        return dp[i] = onechar + twochar;
     }
-
-private:
-    bool isValidTwoDigit(const std::string& twoDigits) {
-        int value = std::stoi(twoDigits);
-        return value >= 10 && value <= 26;
+    
+    
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int>dp(n+1, -1);
+        return solve(0,n,s, dp);
     }
 };
