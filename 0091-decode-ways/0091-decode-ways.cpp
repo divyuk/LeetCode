a155baf599dfd7b7f424a8a43275743c1b5d4073
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-09 11:24:59 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.29%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -19,7 +19,20 @@ public:
     
     int numDecodings(string s) {
         int n = s.size();
-        vector<int>dp(n+1, -1);
-        return solve(0,n,s, dp);
+        vector<int>dp(n+1, 0);
+        dp[n] =  1;
+        for(int i = n-1 ;i>=0; i--){
+            if(s[i] == '0')  dp[i] = 0;
+            else{
+               dp[i] = dp[i+1];
+                if(i+1<n){
+                if(s[i] == '1' or (s[i] == '2' and s[i+1] <='6') ) 
+                    dp[i] += dp[i+2];
+                }
+            }
+          
+        }
+        
+        return dp[0];
     }
 };
