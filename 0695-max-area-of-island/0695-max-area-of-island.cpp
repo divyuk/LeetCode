158eead696f6429c93370b3b14f21f2814776620
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-11 09:45:53 +0530

    Time: 17 ms (36.64%), Space: 28.6 MB (26.25%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,38 +1,38 @@
 class Solution {
-public:
-    bool valid(int i , int j , int r, int c ,vector<vector<int>>& grid ){
-        if(i>=0 and i < r and j>=0 and j<c and grid[i][j] == 1)
-            return true;
-        return false;
+private:
+    bool valid(int i, int j, vector<vector<int>>& grid, int r , int c){
+        return (i>=0 and i<r and j>=0 and j<c and grid[i][j] == 1);
     }
-    void dfs(vector<vector<int>>& grid , int r, int c, int i , int j , int &count){
+    
+    void dfs(int i, int j, vector<vector<int>>& grid, int r , int c, int &count){
         grid[i][j] = 0;
         count++;
-        if(valid(i-1,j,r,c,grid))
-            dfs(grid , r,c,i-1,j,count);
-        if(valid(i+1,j,r,c,grid))
-            dfs(grid , r,c,i+1,j,count);
-        if(valid(i,j-1,r,c,grid))
-            dfs(grid , r,c,i,j-1,count);
-        if(valid(i,j+1,r,c,grid))
-            dfs(grid , r,c,i,j+1,count);
+        vector<pair<int,int>>directions = {{1,0} , {-1,0} , {0,1} , {0,-1}};
+        
+        for(pair<int,int> dir : directions){
+            int newR = i + dir.first;
+            int newC = j + dir.second;
+            if(valid(newR , newC , grid, r,c)){
+                dfs(newR , newC , grid, r,c,count);
+            }
+        }
     }
+    
+    
+public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        int count = 0 ;
-        int ans = 0 ;
-        
-        for(int i = 0 ; i < r ; i++){
-            for(int  j = 0  ; j < c ;j++){
+        int maxArea = 0;int count =0;
+        for(int i = 0 ; i< r; i++){
+            for(int j =0 ; j< c; j++){
                 if(grid[i][j] == 1){
-                    cout<<c<<"\n";
-                    dfs(grid,r,c,i,j,count);
-                    ans = max(ans , count);
-                    count = 0;
+                    dfs(i,j, grid,r,c,count);
+                    maxArea = max(maxArea, count);
+                    count=0;
                 }
             }
         }
-        return ans;
+        return maxArea;
     }
 };
