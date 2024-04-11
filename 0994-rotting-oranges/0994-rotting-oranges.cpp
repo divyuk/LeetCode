ac47e6e7fab3409c6645a5c3522215f2c97e5b61
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-11 10:12:07 +0530

    Time: 7 ms (42.25%), Space: 16.2 MB (23.20%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,62 +1,53 @@
-#include <vector>
-#include <queue>
-
 class Solution {
 public:
-    bool isValid(vector<vector<int>>& grid, int r, int c, int cx, int cy) {
-        return (cx >= 0 && cy >= 0 && cx < r && cy < c && grid[cx][cy] == 1);
-    }
-
     int orangesRotting(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        queue<pair<int, int>> q;
-        int time = 0, fresh = 0;
-
-        // Starting filling the queue with the source (rotten oranges)
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 2)
-                    q.push({i, j});
-                else if (grid[i][j] == 1)
+        queue<pair<int,int>>q;
+        int fresh =0;
+        for(int i = 0; i< r ; i++){
+            for(int j = 0; j< c; j++){
+                if(grid[i][j] == 2)
+                    q.push({i,j});
+                else if(grid[i][j] == 1)
                     fresh++;
             }
         }
         
-        if (fresh == 0) return 0;
-
-        while (!q.empty()) {
-            int size = q.size();  // Calculate the current size of the queue
+        if(fresh==0) return 0;
+        
+        int time = 0;
+        while(!q.empty()){
+            int size = q.size();
             int t = 0;
-            
-            // Take out the first orange from the queue
-            while (size > 0) {
+            while(size--){
+                
                 int x = q.front().first;
                 int y = q.front().second;
                 q.pop();
-                int dx[4] = {1, -1, 0, 0};
-                int dy[4] = {0, 0, 1, -1};
                 
-                for (int i = 0; i < 4; i++) {
-                    int move_x = x + dx[i];
-                    int move_y = y + dy[i];
-                    if (isValid(grid, r, c, move_x, move_y)) {
-                        q.push({move_x, move_y});
+                vector<pair<int,int>> directions = {{1,0} , {-1,0} , {0,1} , {0,-1}};
+                
+                for(pair<int,int> dir : directions){
+                    int newR = x + dir.first;
+                    int newC  = y + dir.second;
+                    if(newR >=0 and newR<r and newC>=0 and newC < c and grid[newR][newC] == 1){
+                        grid[newR][newC] = 2;
+                        q.push({newR , newC});
                         t++;
-                        grid[move_x][move_y] = 2;
                     }
                 }
-                size--;
+                
             }
-            
-            if (t) time++;
+            if(t) time++;    
         }
-         for(int i = 0 ; i<r;i++){
-            for(int j = 0 ; j<c; j++){
-                if(grid[i][j] == 1)
-                    return -1;
+        for(int i = 0; i< r ; i++){
+            for(int j = 0; j< c; j++){
+                if(grid[i][j] == 1) return -1;
             }
         }
-        return  time; 
+        
+        return time;
+        
     }
 };
