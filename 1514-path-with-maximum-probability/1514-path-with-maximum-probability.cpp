a6f0f7b5b098aaa6023ff694f1eab6c3f27fcca1
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-12 11:22:15 +0530

    Time: 137 ms (52.28%), Space: 73.8 MB (25.17%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,45 +1,39 @@
-#include <vector>
-#include <queue>
-#include <utility>
-
-using namespace std;
-
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<vector<pair<int, double>>> graph(n);
-        for (int i = 0; i < edges.size(); ++i) {
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start,int end){
+       
+        // data into adjList
+        unordered_map<int, vector<pair<int, double> >> adj;
+        for(int i =0; i<edges.size() ; i++){
             int u = edges[i][0];
             int v = edges[i][1];
-            double p = succProb[i];
-            graph[u].push_back({v, p});
-            graph[v].push_back({u, p});
+            double prob = succProb[i];
+            adj[u].push_back({v,prob});
+            adj[v].push_back({u,prob});
         }
-
-        vector<double> prob(n, 0.0);
-        prob[start] = 1.0;
-
-        priority_queue<pair<double, int>> pq;
-        pq.push({1.0, start});
-
-        while (!pq.empty()) {
-            auto [curProb, node] = pq.top();
+        
+        typedef pair<double,int>P;
+        priority_queue<P> pq;
+        vector<double>ans(n,0);
+        ans[start] = 1;
+        pq.push({1,start});
+        
+        while(!pq.empty()){
+            int node = pq.top().second;
+            double currp = pq.top().first;
             pq.pop();
-
-            if (node == end) {
-                return curProb;
-            }
-
-            for (auto [neighbor, edgeProb] : graph[node]) {
-                double newProb = curProb * edgeProb;
-
-                if (newProb > prob[neighbor]) {
-                    prob[neighbor] = newProb;
-                    pq.push({newProb, neighbor});
+            for(auto x : adj[node]){
+                int vertex = x.first;
+                double prob =  x.second;
+                double newProb = prob*currp;
+                if(ans[vertex] < newProb){
+                    ans[vertex] = newProb;
+                    pq.push({newProb, vertex });
                 }
+                
             }
         }
-
-        return 0.0;
+        return ans[end];
     }
 };
+
