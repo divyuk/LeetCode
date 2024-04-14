e111e6b25a436b61c1ed59d7396e8b99bc374015
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-14 17:18:01 +0530

    Time: 34 ms (49.45%), Space: 25.3 MB (49.82%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.cpp
+++ 0554-brick-wall/0554-brick-wall.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
         unordered_map<int, int> map;
-        
+        int ans =0;
         for (const auto& row : wall) {
             int sum = 0;
             for (int i = 0; i < row.size() - 1; i++) {
                 sum += row[i];
-                if (map.find(sum) != map.end())
-                    map[sum]++;
-                else
-                    map[sum] = 1;
+                map[sum]++;
+                ans = max(ans, map[sum]);
             }
         }
-        
-        int res = wall.size();
-        for (const auto& pair : map)
-            res = min(res, static_cast<int>(wall.size()) - pair.second);
-        
-        return res;
+        return wall.size() - ans;
     }
 };
