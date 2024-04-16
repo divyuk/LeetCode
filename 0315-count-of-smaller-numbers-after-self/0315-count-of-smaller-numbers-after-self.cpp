f46f0fade56122a2f2a3f514e0fc92301581c9e8
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-16 15:01:19 +0530

    Time: 573 ms (37.21%), Space: 296.5 MB (23.58%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,29 +1,4 @@
 class Solution {
-public:
-    vector<int> countSmaller(vector<int>& nums) {
-        vector<int> counts(nums.size(), 0);
-        vector<pair<int, int>> indexedNums(nums.size());
-        for (int i = 0; i < nums.size(); ++i) {
-            indexedNums[i] = {nums[i], i};
-        }
-        
-        mergeSort(indexedNums, counts);
-        return counts;
-    }
-    
-    void mergeSort(vector<pair<int, int>>& indexedNums, vector<int>& counts) {
-        if (indexedNums.size() <= 1) return;
-        
-        int mid = indexedNums.size() / 2;
-        vector<pair<int, int>> left(indexedNums.begin(), indexedNums.begin() + mid);
-        vector<pair<int, int>> right(indexedNums.begin() + mid, indexedNums.end());
-        
-        mergeSort(left, counts);
-        mergeSort(right, counts);
-        
-        merge(indexedNums, left, right, counts);
-    }
-    
     void merge(vector<pair<int, int>>& indexedNums, vector<pair<int, int>>& left,
                vector<pair<int, int>>& right, vector<int>& counts) {
         int i = 0, j = 0, k = 0;
@@ -52,4 +27,31 @@ public:
             ++k;
         }
     }
+    void mergeSort(vector<pair<int, int>>& indexedNums, vector<int>& counts) {
+        if (indexedNums.size() <= 1) return;
+        
+        int mid = indexedNums.size() / 2;
+        vector<pair<int, int>> left(indexedNums.begin(), indexedNums.begin() + mid);
+        vector<pair<int, int>> right(indexedNums.begin() + mid ,indexedNums.end());
+        
+        mergeSort(left, counts);
+        mergeSort(right, counts);
+        
+        merge(indexedNums, left, right, counts);
+    }
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> counts(nums.size(), 0);
+        vector<pair<int, int>> indexedNums(nums.size());
+        for (int i = 0; i < nums.size(); ++i) {
+            indexedNums[i] = {nums[i], i};
+        }
+        
+        mergeSort(indexedNums, counts);
+        return counts;
+    }
+    
+
+    
+
 };
