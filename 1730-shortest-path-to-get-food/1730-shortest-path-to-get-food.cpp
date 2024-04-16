8da44cd9230db1c6d9973a5982348f0a1d7fd1c4
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-16 21:47:27 +0530

    Time: 62 ms (40.31%), Space: 25.2 MB (32.40%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <queue>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int getFood(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        const int INF = 2147483647;  // Max value for INF
+
+        vector<vector<int>> mat(m, vector<int>(n, INF));
+
+        queue<pair<int, int>> q;
+        int x_ = 0, y_ = 0;
+
+        // Initialize the queue with food cells
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '#') {
+                    q.push({i, j});
+                    mat[i][j] = 0;
+                }
+                if(grid[i][j] == '*'){
+                    x_ = i;
+                    y_ = j;
+                }
+            }
+        }
+
+        vector<pair<int, int>> dirs {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while(!q.empty()){
+            auto [x, y] = q.front();
+            q.pop();
+
+            for(const auto& dir : dirs){
+                int newX = x + dir.first;
+                int newY = y + dir.second;
+
+                if(newX >= 0 && newX < m && newY >= 0 && newY < n && grid[newX][newY] != 'X'){
+                    if(mat[newX][newY] > mat[x][y] + 1) {
+                        mat[newX][newY] = mat[x][y] + 1;
+                        q.push({newX, newY});
+                    }
+                }
+            }
+        }
+
+        return mat[x_][y_] == INF ? -1 : mat[x_][y_];
+    }
+};
