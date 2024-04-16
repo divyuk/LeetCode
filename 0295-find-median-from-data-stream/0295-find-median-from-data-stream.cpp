0930a79322f6e9f8b324b66bc0fc730c7b2dcb53
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-16 18:27:58 +0530

    Time: 466 ms (6.80%), Space: 128.6 MB (9.56%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,38 +1,89 @@
-class MedianFinder {
+class MedianFinder {    
+
+/* The idea of dividing existing numbers into several ranges:
+
+   Say we already have 10k numbers in vector, 
+   each time adding a number requires sorting all 10k numbers, which is slow.
+
+   To optimize, we can store 10k numbers in several (say 10) vectors, 
+   and nums in each vector are sorted.
+   
+   Then each time we add a number, just need to find one vector with correct range,
+   insert the number and sort this vector only. Since its size is relatively small, it's fast.
+   
+   When we have a vector's size greater than a threshold, just split it into two halfs.
+   
+*/
+
 public:
-    priority_queue<int> left_max_heap; //max heap
-    priority_queue<int, vector<int>, greater<int>> right_min_heap; //min heap
+    vector<vector<int>*> raid; // store all ranges
+    int total_size;
+
     MedianFinder() {
-        
+        total_size=0;
+        raid.push_back(new vector<int> ());
     }
     
     void addNum(int num) {
-        if(left_max_heap.empty() || num < left_max_heap.top()) left_max_heap.push(num);
-         else  right_min_heap.push(num);
+        vector<int>* correctRange=NULL;
+        int targetIndex;
         
-        //always maintain left_max_heap size one greater than rigfht_min_heap size
-        //else both should be equal
-        if(abs((int)left_max_heap.size() - (int)right_min_heap.size()) > 1) {
-            right_min_heap.push(left_max_heap.top());
-            left_max_heap.pop();
-        } else if(left_max_heap.size() < right_min_heap.size()) {
-            left_max_heap.push(right_min_heap.top());
-            right_min_heap.pop();
+        // find the correct range to insert given num
+        for (int i=0; i<raid.size(); i++)
+            if ( raid.size()==1 ||
+                 (i==0 && num<=raid[i]->back()) || 
+                 (i==raid.size()-1 && num>=raid[i]->at(0)) ||
+                 (raid[i]->at(0)<=num && num<=raid[i]->back()) ||
+                 (num > raid[i]->back() && num < raid[i+1]->front()) )
+            {
+                correctRange = raid[i];
+                targetIndex = i;
+                break;
+            }
+        
+        // put num at back of correct range, and sort it to keep increasing sequence
+        total_size++;
+        correctRange->push_back(num);
+        sort(correctRange->begin(), correctRange->end());
+                
+        // if current range's size > threshold, split it into two halfs and add them back to this.raid
+        const int max_size = 30;
+        int len = correctRange->size();
+        if (len > max_size) {
+            vector<int> *half1 = new vector<int>(correctRange->begin(), correctRange->begin()+len/2);
+            vector<int> *half2 = new vector<int>(correctRange->begin()+len/2, correctRange->end());
+            
+            delete correctRange;
+            raid[targetIndex]=half2;
+            raid.insert(raid.begin() + targetIndex, half1);
         }
         
     }
-    
+
+    // iterate thru all ranges in this.raid to find median value
     double findMedian() {
-        if(left_max_heap.size() == right_min_heap.size()) //even case
-            return (double)(left_max_heap.top()+right_min_heap.top())/2;
-        //odd case
-        return left_max_heap.top();
+        if (total_size==0)
+            return 0;
+        
+        int mid1 = total_size/2;
+        int mid2 = mid1 + 1;
+        
+        int leftCount=0;
+        double first, second;
+        for (auto r : raid) {
+            if (leftCount<mid1 && mid1<=leftCount+r->size())
+                first = r->at(mid1 - leftCount - 1);
+                
+            if (leftCount<mid2 && mid2<=leftCount+r->size()) {
+                second = r->at(mid2 - leftCount - 1);
+                break;
+            }
+            leftCount += r->size();
+        }
+        
+        if (total_size % 2)
+            return second;
+        else
+            return (first + second)/2;
     }
 };
-
-/** run on 1 3 4 5 7 9
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
