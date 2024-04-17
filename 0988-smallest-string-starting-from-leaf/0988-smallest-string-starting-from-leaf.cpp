1cb387cea1bd3ccaeda5d6e4ad4d4c4acea04220
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-17 07:25:49 +0530

    Time: 3 ms (98.00%), Space: 20.5 MB (51.36%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.cpp
@@ -2,32 +2,20 @@ class Solution {
 private:
         string smallestString;
         void dfs(TreeNode* root, string currentString) {
-
-
             if (!root)   return;
-
-
-            // Construct the current string by appending 
-            // the character corresponding to the node's value
             currentString = char(root->val + 'a') + currentString; 
 
             if (!root->left && !root->right) { 
                 // If the current string is smaller than the result 
                 // or if the result is empty
-                if (smallestString == "" || smallestString > currentString) { 
+                if (smallestString == "" || smallestString > currentString)
                     smallestString = currentString;
-                }
+                
             }
 
-            // Recursively traverse the left subtree
-            // if (root->left) {
-                dfs(root->left, currentString);
-            // }
-
-            // Recursively traverse the right subtree
-            if (root->right) {
-                dfs(root->right, currentString);
-            }
+            dfs(root->left, currentString);
+            dfs(root->right, currentString);
+            
         }
 public:
         string smallestFromLeaf(TreeNode* root) {
