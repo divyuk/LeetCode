f66ebe37b2e70711bfc80c7570f493e87f768d84
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-17 15:37:38 +0530

    Time: 157 ms (93.28%), Space: 16.3 MB (87.54%) - LeetHub

---
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<string> addOperators(string num, int target) {
+        vector<string> result;
+        string expr;
+        backtrack(result, expr, num, target, 0, 0, 0);
+        return result;
+    }
+
+private:
+    void backtrack(vector<string>& result, string& expr, const string& num, int target,
+                   int start, long long currVal, long long prevNum) {
+        if (start == num.size()) {
+            if (currVal == target) {
+                result.push_back(expr);
+            }
+            return;
+        }
+
+        for (int i = start; i < num.size(); ++i) {
+            // Handle cases where we have a number with leading zero
+            if (i > start && num[start] == '0') {
+                break;
+            }
+
+            string currStr = num.substr(start, i - start + 1);
+            long long curr = stoll(currStr);
+
+            if (start == 0) {
+                expr += currStr;
+                backtrack(result, expr, num, target, i + 1, curr, curr);
+                expr.erase(expr.size() - currStr.size());
+            } else {
+                // Addition
+                expr += "+" + currStr;
+                backtrack(result, expr, num, target, i + 1, currVal + curr, curr);
+                expr.erase(expr.size() - 1 - currStr.size());
+
+                // Subtraction
+                expr += "-" + currStr;
+                backtrack(result, expr, num, target, i + 1, currVal - curr, -curr);
+                expr.erase(expr.size() - 1 - currStr.size());
+
+                // Multiplication
+                expr += "*" + currStr;
+                backtrack(result, expr, num, target, i + 1, currVal - prevNum + prevNum * curr, prevNum * curr);
+                expr.erase(expr.size() - 1 - currStr.size());
+            }
+        }
+    }
+};
