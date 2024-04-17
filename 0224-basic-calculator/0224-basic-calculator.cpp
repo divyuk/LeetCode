78157ebfa51d6da8211b27924d566fd4dd68a1ef
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-17 20:26:33 +0530

    Time: 38 ms (11.08%), Space: 285.4 MB (5.02%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    int i=0; // Global variable
+
+public:
+    int calculate(string s) {
+        int operand = 0;
+        int result = 0; // For the on-going result
+        int nextOperandSign = 1;  // 1 means positive, -1 means negative
+        while (i < s.length()) {
+            char ch = s[i++];
+
+            if (ch == ' ' || isdigit(ch)) {
+                operand = (ch == ' ') ? operand : 10 * operand + (ch - '0');
+            } else if (ch == '(') {
+                operand = calculate(s);
+            } else if (ch == ')') {
+                break; // Sub-expression we were evaluating has ended. Exit now...
+            } else {
+                // If we're here, we just read the operator for the next operand/expression.
+                // Evaluate the existing expression already read, reset operand, set sign for next incoming operand.
+                result += nextOperandSign * operand;
+                nextOperandSign = (ch == '+') ? 1 : -1;
+                operand = 0;
+            }
+        }
+
+        return result + (nextOperandSign * operand);
+    }
+};
