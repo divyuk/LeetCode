db307ecf4fc6f299da9b17884a14ba6ae9e006b6
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-18 11:37:45 +0530

    Time: 22 ms (85.21%), Space: 19.8 MB (90.93%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -8,7 +8,6 @@ public:
 };
 */
 
-
 class Solution {
 public:
     int getImportance(vector<Employee*> employees, int id) {
@@ -18,24 +17,34 @@ public:
             employeeMap[employee->id] = employee;
         }
         
-        // Initialize the total importance
-        int totalImportance = 0;
-        
-        // Perform BFS starting from the given employee ID
-        queue<int> q;
-        q.push(id);
-        
-        while (!q.empty()) {
-            int currId = q.front();
-            q.pop();
-            
-            // Add the importance of the current employee
-            totalImportance += employeeMap[currId]->importance;
-            
-            // Add the subordinates of the current employee to the queue
-            for (int subId : employeeMap[currId]->subordinates) {
-                q.push(subId);
+        // print here the employeeMap
+       cout << "Employee Map:\n";
+        for (auto it = employeeMap.begin(); it != employeeMap.end(); ++it) {
+            cout << "ID: " << it->first << ", Importance: " << it->second->importance << ", Subordinates: ";
+            for (int subId : it->second->subordinates) {
+                cout << subId << " ";
             }
+            cout << endl;
+        }
+        
+        // Start DFS from the given employee ID
+        return dfs(employeeMap, id);
+    }
+    
+private:
+    int dfs(unordered_map<int, Employee*>& employeeMap, int id) {
+        // Base case: if the employee is not found, return 0
+        if (employeeMap.find(id) == employeeMap.end()) return 0;
+        
+        
+        // Get the employee object
+        Employee* employee = employeeMap[id];
+        int totalImportance = employee->importance;
+        
+        // Traverse through the direct subordinates and recursively calculate their importance
+        for (int subordinateId : employee->subordinates) {
+            totalImportance += dfs(employeeMap, subordinateId);
+            // cout<<totalImportance<<"\n";
         }
         
         return totalImportance;
