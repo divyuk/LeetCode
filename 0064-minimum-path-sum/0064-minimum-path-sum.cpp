2d0c871f7d103a8d4543135681ca04be24563d45
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-22 18:53:44 +0530

    Time: 11 ms (16.15%), Space: 12.7 MB (14.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -7,13 +7,12 @@ public:
         
         dp[0][0] = grid[0][0];
         
-        for (int i = 1; i < r; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
+        // Filling the Column
+        for (int i = 1; i < r; i++) dp[i][0] = dp[i - 1][0] + grid[i][0];
         
-        for (int j = 1; j < c; j++) {
-            dp[0][j] = dp[0][j - 1] + grid[0][j];
-        }
+        // Filling the Row
+        for (int j = 1; j < c; j++) dp[0][j] = dp[0][j - 1] + grid[0][j];
+        
         
         for (int i = 1; i < r; i++) {
             for (int j = 1; j < c; j++) {
