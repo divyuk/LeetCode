380ee21c0ce5bf46d95ca44d12dd2f876ee06d38
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-22 10:19:52 +0530

    Time: 34 ms (69.29%), Space: 10.5 MB (74.73%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int answer = 0;
+
+    void maxRequest(vector<vector<int>>& requests, vector<int>& indegree, int n, int index, int count) {
+        if (index == requests.size()) {
+            // Check if all buildings have an in-degree of 0.
+            for (int i = 0; i < n; i++) {
+                if (indegree[i]) return;    
+            }
+            answer = max(answer, count);
+            return;
+        }
+        
+     
+        indegree[requests[index][0]]--;
+        indegree[requests[index][1]]++;
+       
+        maxRequest(requests, indegree, n, index + 1, count + 1);
+        // Backtrack
+        indegree[requests[index][0]]++;
+        indegree[requests[index][1]]--;
+        // Ignore this request 
+        maxRequest(requests, indegree, n, index + 1, count);
+    }
+    
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        vector<int> indegree(n, 0);
+        maxRequest(requests, indegree, n, 0, 0);
+        return answer;
+    }
+};
