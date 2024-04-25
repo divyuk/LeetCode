5d9eec37eb94ab0ca826dafd5e374af0c55ac82b
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-25 16:55:32 +0530

    Time: 242 ms (38.72%), Space: 155.3 MB (41.69%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -19,7 +19,16 @@ private:
 public:
     long long maxAlternatingSum(vector<int>& nums) {
         bool flag = true;
-        vector<vector<ll>> dp(nums.size(), vector<ll>(2, -1));
-        return f(nums, 0, flag, dp);
+        int n = nums.size();
+        // vector<vector<ll>> dp(n+1, vector<ll>(2, -1));
+        // return f(nums, 0, flag, dp);
+        vector<vector<ll>> dp(n+1, vector<ll>(2, 0)); // 0: even 1 : odd
+        
+        for(int i = 1; i<n+1 ; i++){
+            // Even
+            dp[i][0] = max(dp[i-1][1] - nums[i-1]  , dp[i-1][0]  );
+            dp[i][1] = max(dp[i-1][0] + nums[i-1] , dp[i-1][1] );
+        }
+        return max(dp[n][0] , dp[n][1]);
     }
 };
