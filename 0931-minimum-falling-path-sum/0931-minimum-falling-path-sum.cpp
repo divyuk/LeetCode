55ced817d139ce60f067218991de20449df3635b
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-26 09:57:25 +0530

    Time: 12 ms (59.15%), Space: 13.5 MB (49.37%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,7 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
-
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
@@ -9,9 +5,8 @@ public:
         vector<vector<int>> dp(n, vector<int>(n, 0));
 
         // Initialize the first row of dp with values from matrix
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = matrix[0][j];
-        }
+        for (int j = 0; j < n; j++) dp[0][j] = matrix[0][j];
+        
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -20,22 +15,19 @@ public:
 
                 // Choose the minimum from the previous row
                 int best_prev = dp[i - 1][j];
-                if (j > 0) {
-                    best_prev = min(best_prev, dp[i - 1][j - 1]);
-                }
-                if (j < n - 1) {
-                    best_prev = min(best_prev, dp[i - 1][j + 1]);
-                }
-
+                
+                if (j > 0) best_prev = min(best_prev, dp[i - 1][j - 1]);
+                
+                if (j < n - 1) best_prev = min(best_prev, dp[i - 1][j + 1]);
+                
                 dp[i][j] += best_prev;
             }
         }
 
         // Find the minimum value in the last row
         int min_sum = INT_MAX;
-        for (int j = 0; j < n; j++) {
-            min_sum = min(min_sum, dp[n - 1][j]);
-        }
+        for (int j = 0; j < n; j++) min_sum = min(min_sum, dp[n - 1][j]);
+   
 
         return min_sum;
     }
