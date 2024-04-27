1d313d2c030da51638755e7b1ef6a445c145ce0a
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-27 18:54:43 +0530

    Time: 3 ms (90.91%), Space: 8.5 MB (97.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,16 @@
 class Solution {
-public:
-    bool isPalindrome(string s) {
-        vector<char>c;
-        for(char i : s){
-            if((i>=65 and i<=90) or (i>=97 and i<=122) ){
-                i= char(tolower(i));
-                c.push_back(i);
-            }
-            else if(i>=48 and i<=57)
-                c.push_back(i);
-            
-        }
-        cout<<int('0')<<"\n";
-        for(auto k : c)
-            cout<<k<<" ";
-        int n = c.size();
-        int i =0,j=n-1;
-        while(i<=j){
-            if(c[i] != c[j])
-                return 0;
-            i++,j--;
-        }
-        return 1;
+ public:
+  bool isPalindrome(string s) {
+    for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+      while (i < j && !isalnum(s[i]))
+        i++;
+      while (i < j && !isalnum(s[j]))
+        j--;
+
+      if (tolower(s[i]) != tolower(s[j]))
+        return false;
     }
+
+    return true;
+  }
 };
