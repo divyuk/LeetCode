d3019373c4abfa942fd2a3a85540fc1865721996
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-27 15:56:08 +0530

    Time: 167 ms (16.75%), Space: 13.7 MB (69.10%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.cpp
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -25,7 +25,29 @@ public:
     int findRotateSteps(string ring, string key) {
         int ringLen = ring.size();
         int keyLen  = key.size();
-        vector<vector<int>>dp(ringLen, vector<int>(keyLen, -1));
-        return solve(ring, key, ringLen, keyLen, 0, 0, dp );
+//         vector<vector<int>>dp(ringLen, vector<int>(keyLen, -1));
+//         return solve(ring, key, ringLen, keyLen, 0, 0, dp );
+        
+        
+        vector<vector<int>>dp(ringLen, vector<int>(keyLen+1, INT_MAX));
+        
+        for(int r =0;r<ringLen;r++) dp[r][keyLen] = 0;
+        
+        for(int k = keyLen -1 ; k>=0 ; k--){
+            for(int r = 0; r<ringLen; r++){
+                int res = INT_MAX;
+                for(int i = 0; i<ringLen; i++){
+                    if(ring[i] == key[k]){
+                        int steps = 1 + countSteps(r,i,ringLen);
+                        int totalSteps = steps + dp[i][k+1];
+                        res = min(res, totalSteps);
+                    }
+                     
+                }
+                dp[r][k] = res;
+            }
+           
+        }
+        return dp[0][0];
     }
 };
