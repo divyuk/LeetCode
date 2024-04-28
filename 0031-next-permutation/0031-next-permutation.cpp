107d379a4f28281a25de9814710117668391d238
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-28 12:19:39 +0530

    Time: 3 ms (64.73%), Space: 14.4 MB (87.22%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size(), k, l;
+        
+        // FInd the dip
     	for (k = n - 2; k >= 0; k--) {
-            if (nums[k] < nums[k + 1]) {
-                break;
-            }
+            if (nums[k] < nums[k + 1]) break;
+            
         }
-    	if (k < 0) {
-    	    reverse(nums.begin(), nums.end());
-    	} else {
+        
+        // If no dip then you are at last permutation, simply reverse back
+        
+    	if (k < 0)  reverse(nums.begin(), nums.end());
+    	else {
     	    for (l = n - 1; l > k; l--) {
-                if (nums[l] > nums[k]) {
-                    break;
-                }
+                if (nums[l] > nums[k]) break;
+                
             } 
     	    swap(nums[k], nums[l]);
     	    reverse(nums.begin() + k + 1, nums.end());
