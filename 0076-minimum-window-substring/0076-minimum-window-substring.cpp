3640730e105c1afd5a4a1403ea3b5ef12b2deace
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-04-28 15:30:44 +0530

    Time: 31 ms (23.63%), Space: 10.9 MB (17.96%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,17 +1,40 @@
 class Solution {
 public:
-string minWindow(string s, string t) {
-        vector<int> map(128,0);
-        for(auto c: t) map[c]++;
-        int counter=t.size(), begin=0, end=0, d=INT_MAX, head=0;
-    
-        while(end<s.size()){
-            if(map[s[end++]]-- > 0) counter--; //in t
-            while(counter==0){ //valid
-                if(end-begin<d)  d=end-(head=begin);
-                if(map[s[begin++]]++==0) counter++;  //make it invalid
-            }  
+    string minWindow(string s, string t) {
+        int n = s.length();
+        map<char, int> mp;
+        
+        for(char &ch : t) {
+            mp[ch]++;
         }
-        return d==INT_MAX? "":s.substr(head, d);
+        
+        int requiredCount = t.length();
+        int i = 0, j  = 0;
+        int minStart  = 0;
+        int minWindow = INT_MAX;
+        while(j < n) {
+            char ch_j = s[j];
+            if(mp[ch_j] > 0)
+                requiredCount--;
+            
+            mp[ch_j]--;
+            
+            while(requiredCount == 0) { //try to shrink the window
+                if(minWindow > j-i+1) {
+                    minWindow = j-i+1;
+                    minStart  = i;
+                }
+                
+                char ch_i = s[i];
+                mp[ch_i]++;
+                if(mp[ch_i] > 0)
+                    requiredCount++;
+                i++;
+            }
+            
+            j++; //Don't ever forget this :-)
+        }
+        
+        return minWindow == INT_MAX ? "" : s.substr(minStart, minWindow);
     }
 };
