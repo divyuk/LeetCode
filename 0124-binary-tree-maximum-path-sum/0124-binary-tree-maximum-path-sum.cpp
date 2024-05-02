4f95a7189d3f723540c2fe3828f8a86427f5df26
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-02 21:13:34 +0530

    Time: 28 ms (6.66%), Space: 26.3 MB (25.70%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -20,11 +20,9 @@ public:
     int maxPathSumRecursive(TreeNode* node, int& maxSum) {
         if (!node) return 0;
         
-        // Calculate the maximum path sum in the left and right subtrees
         int leftSum = max(maxPathSumRecursive(node->left, maxSum), 0);
         int rightSum = max(maxPathSumRecursive(node->right, maxSum), 0);
         
-        // Update the maximum path sum if the path through the current node is larger
         maxSum = max(maxSum, leftSum + rightSum + node->val);
         
         // Return the maximum sum of paths from the current node
