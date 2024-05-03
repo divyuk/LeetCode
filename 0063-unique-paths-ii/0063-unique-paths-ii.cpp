9565b4690365171e309433009e9fd3f9dd068d76
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-03 15:59:17 +0530

    Time: 0 ms (100.00%), Space: 10.4 MB (8.26%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
@@ -13,35 +10,39 @@ public:
         
         dp[0][0] = 1;
         
+        // Fill the first row and col if there is obstacles
         // Fill the first column
         for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0] == 1) {
-                dp[i][0] = 0;
-            } else {
-                dp[i][0] = dp[i - 1][0];
-            }
+            if (obstacleGrid[i][0] == 1) dp[i][0] = 0;
+            else dp[i][0] = dp[i - 1][0];
+            
         }
         
         // Fill the first row
         for (int j = 1; j < n; j++) {
-            if (obstacleGrid[0][j] == 1) {
-                dp[0][j] = 0;
-            } else {
-                dp[0][j] = dp[0][j - 1];
-            }
+            if (obstacleGrid[0][j] == 1)  dp[0][j] = 0;
+             else  dp[0][j] = dp[0][j - 1];
+            
         }
         
         // Fill the rest of the grid
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
+                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                 else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                
             }
         }
         
         return dp[m - 1][n - 1];
     }
 };
+
+
+/*
+    |
+   _*
+*/
+
+
+
