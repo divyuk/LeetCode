3a0f0d7d0bb18a85e9e793a871f096a17d2505a1
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-03 17:23:14 +0530

    Time: 610 ms (36.32%), Space: 296.5 MB (23.26%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,58 +1,54 @@
 class Solution {
+    void merge(vector<pair<int, int>>& indexedNums, vector<pair<int, int>>& left,
+               vector<pair<int, int>>& right, vector<int>& counts) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.size() && j < right.size()) {
+            if (left[i].first <= right[j].first) {
+                indexedNums[k] = left[i];
+                counts[left[i].second] += j;  // Increment count of smaller elements
+                ++i;
+            } else {
+                indexedNums[k] = right[j];
+                ++j;
+            }
+            ++k;
+        }
+        
+        while (i < left.size()) {
+            indexedNums[k] = left[i];
+            counts[left[i].second] += j;  // Increment count of smaller elements
+            ++i;
+            ++k;
+        }
+        
+        while (j < right.size()) {
+            indexedNums[k] = right[j];
+            ++j;
+            ++k;
+        }
+    }
+    void mergeSort(vector<pair<int, int>>& indexedNums, vector<int>& counts) {
+        if (indexedNums.size() <= 1) return;
+        
+        int mid = indexedNums.size() / 2;
+        vector<pair<int, int>> left(indexedNums.begin(), indexedNums.begin() + mid);
+        vector<pair<int, int>> right(indexedNums.begin() + mid ,indexedNums.end());
+        
+        mergeSort(left, counts);
+        mergeSort(right, counts);
+        
+        merge(indexedNums, left, right, counts);
+    }
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n);
-        vector<int> indices(n);  // record the index. we are going to sort this array
-        for (int i = 0; i < n; i++) {
-            indices[i] = i;
-        }
-        // sort indices with their corresponding values in nums, i.e., nums[indices[i]]
-        mergeSort(indices, 0, n, result, nums);
-        return result;
+        vector<int> counts(nums.size(), 0);
+        vector<pair<int, int>> indexedNums(nums.size());
+        
+        for (int i = 0; i < nums.size(); ++i) indexedNums[i] = {nums[i], i};
+
+        mergeSort(indexedNums, counts);
+        
+        return counts;
     }
 
-    void mergeSort(vector<int>& indices, int left, int right, vector<int>& result,
-                   vector<int>& nums) {
-        if (right - left <= 1) {
-            return;
-        }
-        int mid = (left + right) / 2;
-        mergeSort(indices, left, mid, result, nums);
-        mergeSort(indices, mid, right, result, nums);
-        merge(indices, left, right, mid, result, nums);
-    }
-
-    void merge(vector<int>& indices, int left, int right, int mid, vector<int>& result,
-               vector<int>& nums) {
-        // merge [left, mid) and [mid, right)
-        int i = left;  // current index for the left array
-        int j = mid;   // current index for the right array
-        // use temp to temporarily store sorted array
-        vector<int> temp;
-        while (i < mid && j < right) {
-            if (nums[indices[i]] <= nums[indices[j]]) {
-                // j - mid numbers jump to the left side of indices[i]
-                result[indices[i]] += j - mid;
-                temp.push_back(indices[i]);
-                i++;
-            } else {
-                temp.push_back(indices[j]);
-                j++;
-            }
-        }
-        // when one of the subarrays is empty
-        while (i < mid) {
-            // j - mid numbers jump to the left side of indices[i]
-            result[indices[i]] += j - mid;
-            temp.push_back(indices[i]);
-            i++;
-        }
-        while (j < right) {
-            temp.push_back(indices[j]);
-            j++;
-        }
-        // restore from temp
-        move(temp.begin(), temp.end(), indices.begin() + left);
-    }
 };
