8400d1865cec545dbbd070fb0f6f6c04ecb0b1d4
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-04 12:58:04 +0530

    Time: 513 ms (37.07%), Space: 134.6 MB (35.00%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
+        typedef pair<int,int>P;
         int n = points.size();
-        vector<vector<pair<int, int>>> graph(n);
-
-        // Calculate Manhattan distance between all pairs of points
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                graph[i].emplace_back(j, dist);
-                graph[j].emplace_back(i, dist);
+        unordered_map<int,vector<P>>adj; // i -> {cost,node}
+        int cost =0;
+        for(int i = 0;i<n;i++) {
+            int x1 = points[i][0],y1 = points[i][1];
+            for(int j = i+1; j< n; j++){
+                int x2 = points[j][0], y2 = points[j][1];
+                int w = abs(x2-x1) + abs(y2-y1);
+                adj[i].push_back({w,j});
+                adj[j].push_back({w,i});
             }
         }
+        
+        
+         // Printing the adjacency list
+        // for (auto &[vertex, neighbors] : adj) {
+        //     cout << "Vertex " << vertex << ": ";
+        //     for (auto &[cost, node] : neighbors) {
+        //         cout << "(" << cost << ", " << node << ") ";
+        //     }
+        //     cout << endl;
+        // }
+        
+        // Prims algo 
+        vector<bool>visited(n,false);
+        priority_queue<P,vector<P>, greater<P>>pq;
+        pq.push({0,0});// 0 weight , 0 index;
 
-        // Prim's algorithm
-        int minCost = 0;
-        vector<bool> visited(n, false);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-        minHeap.emplace(0, 0); // (distance, node)
-        while (!minHeap.empty()) {
-            auto [cost, node] = minHeap.top();
-            minHeap.pop();
-            if (!visited[node]) {
+        while(!pq.empty()){
+            P curr = pq.top();
+            int wt = curr.first;
+            int node = curr.second;
+            pq.pop();
+            if(!visited[node]){
+                cost+=wt;
                 visited[node] = true;
-                minCost += cost;
-                for (const auto& [neighbor, dist] : graph[node]) {
-                    if (!visited[neighbor])
-                        minHeap.emplace(dist, neighbor);
+                for(auto i : adj[node]){
+                    int c = i.first;
+                    int v = i.second;
+                    if(!visited[v]) pq.push({c,v});
+                    
                 }
             }
+            
         }
-
-        return minCost;
+        
+        
+        
+       return cost; 
     }
 };
