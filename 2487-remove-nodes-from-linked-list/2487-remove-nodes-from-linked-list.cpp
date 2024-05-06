23b5a318e7d36bd39f58e03c5af475d54fcadb8d
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-06 10:47:59 +0530

    Time: 251 ms (89.71%), Space: 159.8 MB (81.03%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -1,32 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* newHead = NULL;
+        while(head){
+            ListNode* forw = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = forw;
+        }
+        return newHead;
+    }
 public:
     ListNode* removeNodes(ListNode* head) {
-        stack<ListNode*> stack;
+        head = reverseList(head);
+        int maxi = 0;
+        ListNode* prev = NULL;
         ListNode* current = head;
-        while (current) {
-            stack.push(current);
-            current = current->next;
-        }
-
-        current = stack.top();
-        stack.pop();
-        int maximum = current->val;
-        ListNode* resultList = new ListNode(maximum);
-
-        // Remove nodes from the stack and add to result
-        while (!stack.empty()) {
-            current = stack.top();
-            stack.pop();
-            // small
-            if (current->val < maximum) continue;
-            else {
-                ListNode* newNode = new ListNode(current->val);
-                newNode->next = resultList;
-                resultList = newNode;
-                maximum = current->val;
+        while(current){
+            maxi= max(maxi,current->val);
+            if(current->val< maxi){
+                prev->next = current->next;
+                ListNode* del = current;
+                current=current->next;
+                del->next =nullptr;
+            }
+            else{
+                prev=current;
+                current=current->next;
             }
         }
-
-        return resultList;
+        return reverseList(head);
     }
 };
