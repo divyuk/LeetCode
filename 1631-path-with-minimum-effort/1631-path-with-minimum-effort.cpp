dd2d8ffa4055014da20863ea862512abd5f70b50
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-07 18:37:47 +0530

    Time: 244 ms (13.04%), Space: 53.9 MB (13.09%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int row = heights.size();
+        int col = heights[0].size();
+        vector<pair<int,int>> dir{{1,0},{-1,0},{0,1},{0,-1}};
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> min_heap;
+        min_heap.push({0, 0, 0});
+
+        while (!min_heap.empty()) {
+            auto curr = min_heap.top();
+            int dh = curr[0];
+            int i = curr[1];
+            int j = curr[2];
+            min_heap.pop();
+            
+            if (visited[i][j]) continue;
+            if (i == row - 1 && j == col - 1) return dh;
+            visited[i][j] = true;
+            
+            for (auto& p : dir) {
+                int ni = i + p.first;
+                int nj = j + p.second;
+                if (ni >= 0 && ni < row && nj >= 0 && nj < col && !visited[ni][nj]) {
+                    int new_dh = max(dh, abs(heights[ni][nj] - heights[i][j]));
+                    min_heap.push({new_dh, ni, nj});
+                }
+            }
+        }
+        return 0;
+    }
+};
