261b357599cc758c76312a826d3ce046202b5fec
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-07 09:38:48 +0530

    Time: 189 ms (36.79%), Space: 136.3 MB (28.03%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* newHead = nullptr;
+        while (head) {
+            ListNode* forw = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = forw;
+        }
+        return newHead;
+    }
+public:
+    ListNode* doubleIt(ListNode* head) {
+        ListNode* reversed = reverseList(head);
+        ListNode* temp = reversed;
+        ListNode* dummy = new ListNode();
+        ListNode* dummyHead = dummy;
+        int carry = 0;
+        while (temp) {
+            int val = (temp->val * 2) + carry;
+            int digit = val % 10;
+            carry = val / 10;
+            dummy->next = new ListNode(digit);
+            dummy = dummy->next;
+            temp = temp->next;
+        }
+        while (carry > 0) {
+            dummy->next = new ListNode(carry % 10);
+            carry /= 10;
+            dummy = dummy->next;
+        }
+        return reverseList(dummyHead->next);
+    }
+};
