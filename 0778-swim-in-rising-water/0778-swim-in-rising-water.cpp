eedeeb6c26edca766bbecce75aef57b5faa2cd26
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-07 15:32:46 +0530

    Time: 7 ms (94.82%), Space: 12.5 MB (63.43%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,26 +1,33 @@
 class Solution {
+    struct T {
+        int height, r, c;
+        T(int x, int y, int z) : height(x), r(y), c(z){}
+        bool operator< (const T &d) const {return height > d.height;}
+    };
     typedef pair<int, pair<int,int>> P; // {height , i,j}
     vector<pair<int,int>> dir{{1,0},{-1,0},{0,1},{0,-1}};
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
-        priority_queue<P,vector<P>, greater<>> pq; // min heap
+        // priority_queue<P,vector<P>, greater<>> pq; // min heap
+        priority_queue<T>pq;
         vector<vector<bool>>visited(n,vector<bool>(n,false));
         visited[0][0] = true;
-        pq.push({grid[0][0] ,{0,0}});
+        pq.push(T(grid[0][0] , 0 , 0));
+        // pq.push({grid[0][0] ,{0,0}});
         
         while(!pq.empty()){
-            P curr = pq.top();
-            int h = curr.first;
-            int i = curr.second.first;
-            int j = curr.second.second;
+            auto curr = pq.top();
+            int h = curr.height;
+            int i = curr.r;
+            int j = curr.c;
             if(i==n-1 and j == n-1) return h;
             pq.pop();
             for(pair<int,int>p : dir){
                 int x = i + p.first;
                 int y = j + p.second;
                 if(x>=0 and x<n and y>=0 and y<n and !visited[x][y]){
-                    pq.push( {max(grid[x][y], h) , {x,y} }  );
+                    pq.push( T(max(grid[x][y], h) , x,y )  );
                     visited[x][y] = 1;
                 }
             }
