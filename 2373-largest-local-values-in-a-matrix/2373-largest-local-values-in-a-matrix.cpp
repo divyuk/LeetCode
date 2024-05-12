7d3152415803b8abfd40f061fa309faf76c73760
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-12 10:46:12 +0530

    Time: 9 ms (66.10%), Space: 13.6 MB (53.31%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.cpp
@@ -1,45 +1,26 @@
 class Solution {
-
 public:
-
-    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
-
-        int n=grid.size();
-
-        vector<vector<int>> res(n-2, vector<int> (n-2));
-
-        
-
-        //find max of 3x3 grid centred around row (i+1) and column (j+1)
-
-		//for eg:1 starting from (1,1) so (1,1),(1,2),(2,1),(2,2) will be the centre of 3x3 grid-1,2,3,4 respectively
-
-        for(int i=1; i<=n-2; i++)
-
-        {
-
-            for(int j=1; j<=n-2; j++)
-
-            {
-
-                int maxi=0;
-
-                maxi = max({maxi, grid[i-1][j-1], grid[i-1][j], grid[i-1][j+1]});
-
-                maxi = max({maxi, grid[i][j-1], grid[i][j], grid[i][j+1]});
-
-                maxi = max({maxi, grid[i+1][j-1], grid[i+1][j], grid[i+1][j+1]});
-
-                
-
-               res[i-1][j-1] = maxi;
-
+    int findLocalMax(vector<vector<int>>& grid, int x, int y) {
+        int maxElement = 0;
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                maxElement = max(maxElement, grid[i][j]);
             }
-
         }
-
-        return res;
-
+        return maxElement;
     }
-
+    
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> maxLocal(n - 2, vector<int>(n - 2, 0));
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                maxLocal[i][j] = findLocalMax(grid, i, j);
+            }
+        }
+        return maxLocal;
+    }
 };
+//Approach(Simply traversing and finding the result)
+//T.C : O(n*n)
+//S.C : O(n*n) for thr result but you can ignore it because it's the result that we have to return
