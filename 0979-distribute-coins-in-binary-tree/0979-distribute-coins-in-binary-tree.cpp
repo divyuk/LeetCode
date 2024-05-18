72a92fa83dbd493e26bb8078b7b88400ec6c40d4
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-18 18:29:30 +0530

    Time: 5 ms (39.35%), Space: 15.4 MB (89.89%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int moves;
+    int dfs(TreeNode* current) {
+        if (current == nullptr) return 0;
+
+        int leftCoins = dfs(current->left);
+        int rightCoins = dfs(current->right);
+
+        moves += abs(leftCoins) + abs(rightCoins);
+
+        return (current->val - 1) + leftCoins + rightCoins;
+    }
+public:
+    int distributeCoins(TreeNode* root) {
+        moves = 0;
+        dfs(root);
+        return moves;
+    }
+
+};
