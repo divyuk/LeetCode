438b4bd137ce15f401577a3c4481c76ebaf2e02c
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-05-27 09:57:15 +0530

    Time: 5 ms (33.11%), Space: 10.2 MB (19.80%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -9,11 +9,15 @@ public:
         for (int x = 0; x <= n; ++x) {
             // Find the number of elements >= x using lower_bound
             int count = n - (lower_bound(nums.begin(), nums.end(), x) - nums.begin());
-            if (count == x) {
-                return x;
-            }
+            if (count == x)  return x;
         }
-        
         return -1;
     }
 };
+
+//[3,9,7,8,3,8,6,6] n = 8 , o/p : 6
+
+//  0 1 2 3 4 5 6 7 
+// [3 3 6 6 7 8 8 9]
+
+// count = 8 - 2 = 6
