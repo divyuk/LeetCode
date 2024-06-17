e9cee67a98f40482bc387befc80878484b6c54e6
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-06-17 12:04:19 +0530

    Time: 3 ms (68.00%), Space: 7.1 MB (61.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,23 @@
+//T.C : O(sqrt(c))
+//S.C : O(1)
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long a = 0;
+        long b = sqrt(c);
+
+        while(a <= b) {
+            long k = a*a + b*b;
+
+            if(k < c) {
+                a++;
+            } else if (k > c) {
+                b--;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
