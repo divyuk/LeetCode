293b79f7e9063cb6219f7bae514133c90f559453
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-07-20 14:43:22 +0530

    Time: 44 ms (20.54%), Space: 36.2 MB (11.51%) - LeetHub

---
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum,
+                                      vector<int>& colSum) {
+        int N = rowSum.size();
+        int M = colSum.size();
+
+        vector<vector<int>> origMatrix(N, vector<int>(M, 0));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                origMatrix[i][j] = min(rowSum[i], colSum[j]);
+
+                rowSum[i] -= origMatrix[i][j];
+                colSum[j] -= origMatrix[i][j];
+            }
+        }
+
+        return origMatrix;
+    }
+};
