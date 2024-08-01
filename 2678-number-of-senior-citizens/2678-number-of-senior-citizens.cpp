35fe793b4d8614b73f8e8601b324c520dab300d8
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-08-01 12:45:38 +0530

    Time: 8 ms (47.01%), Space: 17.6 MB (27.35%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int seniorCount = 0;
-
-        // Iterate through each passenger's details
         for (string& passengerInfo : details) {
-            // Extract the digits of age
+
             int ageTens = passengerInfo[11] - '0';
             int ageOnes = passengerInfo[12] - '0';
 
-            // Calculate the full age
             int age = ageTens * 10 + ageOnes;
 
-            // Check if the passenger is a senior (strictly over 60 years old)
-            if (age > 60) {
-                seniorCount++;
-            }
+            if (age > 60) seniorCount++;
+            
         }
 
         return seniorCount;
