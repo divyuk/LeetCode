b6a90ebbdf1b7e68c85bfaa54da4d3f981bd8e5a
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-08-03 16:34:07 +0530

    Time: 10 ms (52.84%), Space: 19.7 MB (20.91%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -9,13 +9,9 @@ public:
         for (int num : target) {
             // If num does not appear in target, then arrays are not equal
             if (arrFreq.find(num) == arrFreq.end()) return false;
-
-            // Decrement the frequency count for num and
-            // remove key if the count goes to 0
             arrFreq[num]--;
-            if (arrFreq[num] == 0) {
-                arrFreq.erase(num);
-            }
+            if (arrFreq[num] == 0)  arrFreq.erase(num);
+            
         }
         return arrFreq.size() == 0;
     }
