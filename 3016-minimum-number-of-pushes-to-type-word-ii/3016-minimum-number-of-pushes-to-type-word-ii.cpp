c081340b536ae21841447f5226244f715fd0a173
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-08-06 10:02:31 +0530

    Time: 108 ms (31.41%), Space: 27.2 MB (35.85%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        // Frequency map to store count of each letter
+
         unordered_map<char, int> frequencyMap;
 
-        // Count occurrences of each letter
-        for (char& c : word) {
-            ++frequencyMap[c];
-        }
-
-        // Priority queue to store frequencies in descending order
+        for (char& c : word)  ++frequencyMap[c];
+   
         priority_queue<int> frequencyQueue;
 
-        // Push all frequencies into the priority queue
-        for (const auto& entry : frequencyMap) {
-            frequencyQueue.push(entry.second);
-        }
+        for (const auto& entry : frequencyMap) frequencyQueue.push(entry.second);
+        
 
         int totalPushes = 0;
         int index = 0;
