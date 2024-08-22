0f8c67e83f2c003857512524dbc0dbe9ef5aa226
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-08-22 21:23:53 +0530

    Time: 3 ms (41.07%), Space: 7.6 MB (8.34%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -3,6 +3,7 @@ public:
     int findComplement(int num) {
         if(num==0) return 1;
         int num_bits = log2(num) +1;
+        cout<<num_bits<<" ";
         int mask = (unsigned)(1<<num_bits) - 1;
         return num^mask;
     }
