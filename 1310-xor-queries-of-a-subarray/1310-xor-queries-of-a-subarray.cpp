3f0685b55c1b3057482a03b14650e7f72163ce52
Divyanshu Kesarwani <112690602+divyuk@users.noreply.github.com>
2024-09-13 10:48:21 +0530

    Time: 53 ms (83.46%), Space: 42.2 MB (42.84%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -5,12 +5,10 @@ public:
         vector<int> prefixXOR(n + 1, 0);
 
         for (int i = 0; i < n; i++) prefixXOR[i + 1] = prefixXOR[i] ^ arr[i];
-        
-
+    
         vector<int> result;
-        for (const auto& q : queries) {
-            result.push_back(prefixXOR[q[1] + 1] ^ prefixXOR[q[0]]);
-        }
+        for (const auto& q : queries)  result.push_back(prefixXOR[q[1] + 1] ^ prefixXOR[q[0]]);
+        
         return result;
     }
 };
